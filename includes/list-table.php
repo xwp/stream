349404d6ee9c2ba89c974d4faedb31ab77eda7e4
Frankie Jarrett <frankie@x-team.com>
2013-12-04 23:40:23 -0600

    Use UTC date when filtering records since that's what's in the DB

--- includes/list-table.php
+++ includes/list-table.php
@@ -109,7 +109,7 @@ class WP_Stream_List_Table extends WP_List_Table {
 	function column_default( $item, $column_name ) {
 		switch ( $column_name ) {
 			case 'date':
-				$out  = $this->column_link( get_date_from_gmt( $item->created, 'Y/m/d' ), 'date', get_date_from_gmt( $item->created, 'Y/m/d' ) );
+				$out  = $this->column_link( get_date_from_gmt( $item->created, 'Y/m/d' ), 'date', date( 'Y/m/d', strtotime( $item->created ) ) );
 				$out .= '<br />';
 				$out .= get_date_from_gmt( $item->created, 'h:i:s A' );
 				break;
