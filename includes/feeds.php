d361b06833e68fb2687f01487749cee24cff1489
Frankie Jarrett <frankie@x-team.com>
2013-12-14 22:18:06 -0600

    Code formatting

--- includes/feeds.php
+++ includes/feeds.php
@@ -57,7 +57,7 @@ class WP_Stream_Feeds {
 			<tr>
 				<th><label for="stream_feed_url"><?php esc_html_e( 'Stream Feed URL', 'stream' ) ?></label></th>
 				<td>
-					<code><?php echo add_query_arg( array( self::FEED_QUERY_VAR => $key ), home_url() ) ?></code>
+					<code><?php echo esc_url( add_query_arg( array( self::FEED_QUERY_VAR => $key ), home_url() ) ) ?></code>
 					<p class="description"><?php esc_html_e( 'This is a private URL for you to access your Stream activity.', 'stream' ) ?></p>
 				</td>
 			</tr>
@@ -92,54 +92,54 @@ class WP_Stream_Feeds {
 			echo '<?xml version="1.0" encoding="' . get_option( 'blog_charset' ) . '"?>';
 			?>
 
-<rss version="2.0"
-	xmlns:content="http://purl.org/rss/1.0/modules/content/"
-	xmlns:wfw="http://wellformedweb.org/CommentAPI/"
-	xmlns:dc="http://purl.org/dc/elements/1.1/"
-	xmlns:atom="http://www.w3.org/2005/Atom"
-	xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
-	xmlns:slash="http://purl.org/rss/1.0/modules/slash/"
-<?php do_action( 'rss2_ns' ) ?>
->
-	<channel>
-		<title><?php bloginfo_rss( 'name' ) ?> - <?php esc_html_e( 'Stream Feed', 'stream' ) ?></title>
-		<atom:link href="<?php self_link() ?>" rel="self" type="application/rss+xml" />
-		<link><?php bloginfo_rss( 'url' ) ?></link>
-		<description><?php bloginfo_rss( 'description' ) ?></description>
-		<lastBuildDate><?php echo mysql2date( 'r', $latest_record, false ) ?></lastBuildDate>
-		<language><?php bloginfo_rss( 'language' ) ?></language>
-		<sy:updatePeriod><?php echo apply_filters( 'rss_update_period', 'hourly' ) ?></sy:updatePeriod>
-		<sy:updateFrequency><?php echo apply_filters( 'rss_update_frequency', '1' ) ?></sy:updateFrequency>
-		<?php do_action( 'rss2_head' ) ?>
-<?php foreach ( $records as $record ) : ?>
-<?php
-$record_link  = add_query_arg(
-	array(
-		'page'       => WP_Stream_Admin::RECORDS_PAGE_SLUG,
-		'record__in' => $record->ID,
-	),
-	admin_url( WP_Stream_Admin::ADMIN_PARENT_PAGE )
-);
-$author       = get_userdata( $record->author );
-$display_name = isset( $author->display_name ) ? $author->display_name : 'N/A';
-?>
-		<item>
-			<title><![CDATA[ <?php echo trim( $record->summary ) // xss ok ?> ]]></title>
-			<pubDate><?php echo mysql2date( 'r', $record->created, false ) ?></pubDate>
-			<dc:creator><?php echo esc_html( $display_name ) ?></dc:creator>
-			<category domain="connector"><![CDATA[ <?php echo esc_html( $record->connector ) ?> ]]></category>
-			<category domain="context"><![CDATA[ <?php echo esc_html( $record->context ) ?> ]]></category>
-			<category domain="action"><![CDATA[ <?php echo esc_html( $record->action ) ?> ]]></category>
-			<category domain="ip"><?php echo esc_html( $record->ip ) ?></category>
-			<guid isPermaLink="false"><?php echo esc_url( $record_link ) ?></guid>
-			<link><?php echo esc_url( $record_link ) ?></link>
-<?php rss_enclosure() ?>
-<?php do_action( 'rss2_item' ) ?>
-		</item>
-<?php endforeach; ?>
-	</channel>
-</rss>
-<?php
+			<rss version="2.0"
+				xmlns:content="http://purl.org/rss/1.0/modules/content/"
+				xmlns:wfw="http://wellformedweb.org/CommentAPI/"
+				xmlns:dc="http://purl.org/dc/elements/1.1/"
+				xmlns:atom="http://www.w3.org/2005/Atom"
+				xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
+				xmlns:slash="http://purl.org/rss/1.0/modules/slash/"
+				<?php do_action( 'rss2_ns' ) ?>
+			>
+				<channel>
+					<title><?php bloginfo_rss( 'name' ) ?> - <?php esc_html_e( 'Stream Feed', 'stream' ) ?></title>
+					<atom:link href="<?php self_link() ?>" rel="self" type="application/rss+xml" />
+					<link><?php bloginfo_rss( 'url' ) ?></link>
+					<description><?php bloginfo_rss( 'description' ) ?></description>
+					<lastBuildDate><?php echo mysql2date( 'r', $latest_record, false ) ?></lastBuildDate>
+					<language><?php bloginfo_rss( 'language' ) ?></language>
+					<sy:updatePeriod><?php echo apply_filters( 'rss_update_period', 'hourly' ) ?></sy:updatePeriod>
+					<sy:updateFrequency><?php echo apply_filters( 'rss_update_frequency', '1' ) ?></sy:updateFrequency>
+					<?php do_action( 'rss2_head' ) ?>
+					<?php foreach ( $records as $record ) : ?>
+						<?php
+						$record_link  = add_query_arg(
+							array(
+								'page'       => WP_Stream_Admin::RECORDS_PAGE_SLUG,
+								'record__in' => $record->ID,
+							),
+							admin_url( WP_Stream_Admin::ADMIN_PARENT_PAGE )
+						);
+						$author       = get_userdata( $record->author );
+						$display_name = isset( $author->display_name ) ? $author->display_name : 'N/A';
+						?>
+						<item>
+							<title><![CDATA[ <?php echo trim( $record->summary ) // xss ok ?> ]]></title>
+							<pubDate><?php echo mysql2date( 'r', $record->created, false ) ?></pubDate>
+							<dc:creator><?php echo esc_html( $display_name ) ?></dc:creator>
+							<category domain="connector"><![CDATA[ <?php echo esc_html( $record->connector ) ?> ]]></category>
+							<category domain="context"><![CDATA[ <?php echo esc_html( $record->context ) ?> ]]></category>
+							<category domain="action"><![CDATA[ <?php echo esc_html( $record->action ) ?> ]]></category>
+							<category domain="ip"><?php echo esc_html( $record->ip ) ?></category>
+							<guid isPermaLink="false"><?php echo esc_url( $record_link ) ?></guid>
+							<link><?php echo esc_url( $record_link ) ?></link>
+							<?php rss_enclosure() ?>
+							<?php do_action( 'rss2_item' ) ?>
+						</item>
+					<?php endforeach; ?>
+				</channel>
+			</rss>
+			<?php
 			exit;
 		}
 	}
