d021b42da48868a55c42b6ae9b9c424d8263a13f
Frankie Jarrett <frankie@x-team.com>
2014-01-08 14:02:07 -0600

    Fix fallbacks when remote fetched data exists but is empty

--- includes/extensions.php
+++ includes/extensions.php
@@ -34,7 +34,9 @@ if ( ! empty( $extensions ) ) {
 				$is_active    = ( $plugin_path && is_plugin_active( $plugin_path ) );
 				$is_installed = ( $plugin_path && defined( 'WP_PLUGIN_DIR' ) && file_exists( trailingslashit( WP_PLUGIN_DIR )  . $plugin_path ) );
 				$action_link  = isset( $extension->post_meta->external_url[0] ) ? $extension->post_meta->external_url[0] : $extension->link;
+				$action_link  = ! empty( $action_link ) ? $action_link : $extension->link;
 				$image_src    = isset( $extension->featured_image->source ) ? $extension->featured_image->source : null;
+				$image_src    = ! empty( $image_src ) ? $image_src : null;
 				?>
 
 				<div class="theme<?php if ( $is_installed ) { echo esc_attr( ' active' ); } ?>">
