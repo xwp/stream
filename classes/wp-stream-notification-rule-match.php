cbb2ee9ea4d1760cdfb6d0cf37feb4b55d928ebf
Shady Sharaf <shady@sharaf.me>
2014-01-22 19:06:39 +0200

    Trigger tree traversing finally working, big thanks to @dero

--- classes/wp-stream-notification-rule-match.php
+++ classes/wp-stream-notification-rule-match.php
@@ -47,90 +47,51 @@ class WP_Stream_Notification_Rule_Matcher {
 
 	public function match( $record_id, $log ) {
 
-		$matching_rules = array();
+		$rules = $this->rules();
+		$rule_match = array();
 
-		foreach ( $this->rules() as $rule_id => $rule ) {
-
-			$trigger_groups = $rule['trigger_groups'];
-
-			$group_match = array();
-
-			foreach ( $trigger_groups as $group_idx => $group ) {
-				
-				$trigger_match = array();
-
-				foreach ( $group['triggers'] as $i => $trigger ) {
-
-					$trigger_match[$i] = $this->match_trigger( $trigger, $log );
-
-					if ( ! isset( $last_and_trigger ) ) {
-						$last_and_trigger = null;
-					}
-
-					if ( $this->does_it_fail( $i, $last_and_trigger, $group['triggers'], $trigger_match ) ) {
-						$group_match[ $group_idx ] = false;
-						// Break off triggers loop
-						break;
-					}
-
-					if ( $trigger['relation'] == 'and' ) {
-						$last_and_trigger = $i;
-					}
-				}
-
-				// If we have not failed it before, make it true
-				if ( ! isset( $group_match[ $group_idx ] ) ) {
-					$group_match[ $group_idx ] = true;
-				}
-
-				if ( ! isset( $last_and_group ) ) {
-					$last_and_group = null;
-				}
-
-				if ( $this->does_it_fail( $group_idx, $last_and_group, $trigger_groups, $group_match ) ) {
-					break; // Break off groups loop
-				}
-			}
-
-			// By the time we get here, the rule actually matches
-			if ( ( count( array_filter( $group_match ) ) > 0 ) ) {
-				$matching_rules[] = $rule_id;
-			}
+		foreach ( $rules as $rule_id => $rule ) {
+			$rule_match[ $rule_id ] = $this->match_group( $rule['triggers'], $log );
 		}
 
-			// {echo '<pre>';var_dump($matching_rules);echo '</pre>';die();}
+		$rule_match = array_keys( array_filter( $rule_match ) );
+		$matching_rules = array_intersect_key( $rules, array_flip( $rule_match ) );
 
+		$this->alert( $matching_rules, $log );
 	}
 
-	private function does_it_fail( $index, $last_and, $items, $matches ) {
+	/**
+	 * Match a group of chunked triggers against a log operation
+	 * @param  array  $chunks Chunks of triggers, usually from group[triggers]
+	 * @param  array  $log    Log operation array
+	 * @return bool           Matching result
+	 */
+	private function match_group( $chunks, $log ) {
+		// Separate triggers by 'AND'/'OR' relation, to be able to fail early
+		// and not have to traverse the whole trigger tree
+		foreach ( $chunks as $chunk ) {
+			$results = array();
+			foreach ( $chunk as $trigger ) {
+				$is_group = isset( $trigger['triggers'] );
 
-		$is_last = ( $index + 1 == count( $items ) );
-		$next_is_and = $is_last || ( $items[ $index + 1 ]['relation'] == 'and' );
-
-		$relation_is_and = ( $index == 0 ) || ( $items[$index]['relation'] == 'and' );
-
-		$is_match = $matches[ $index ];
-
-		// If an effective node, last in items, or next item has relation=AND
-		if ( $is_last || $next_is_and ) {
-			if ( $relation_is_and ) {
-				$passed = $is_match;
-			} else {
-				$subset = array_slice( $matches, $last_and );
-				$passed = count( array_filter( $subset ) ) > 0;
+				if ( $is_group ) {
+					$results[] = $this->match_group( $trigger['triggers'], $log );
+				} else {
+					$results[] = $this->match_trigger( $trigger, $log );
+				}
 			}
-
-			if ( ! $passed ) {
-				return true; // Does fail the chain
+			// If the whole chunk fails, fail the whole group
+			if ( count( array_filter( $results ) ) == 0 ) {
+				return false;
 			}
 		}
-
-		return false; // Does not fail, continue the loop
+		// If nothing fails, group matches
+		return true;
 	}
 
 	public function match_trigger( $trigger, $log ) {
 		# DEBUG
-		return ( in_array( $trigger['type'], array( 'object_type' ) ) );
+		return ( in_array( $trigger['type'], array( 'author_role' ) ) );
 	}
 
 	/**
@@ -138,76 +99,151 @@ class WP_Stream_Notification_Rule_Matcher {
 	 * @param  array  $rules Array of rule IDs
 	 * @return array         Reformatted array of groups/triggers
 	 */
-	public function format( $rules ) {
-		return array();
-		/*
+	private function format( $rules ) {
 		$output = array();
 		foreach ( $rules as $rule_id ) {
 			$output[ $rule_id ] = array();
 			$rule = new WP_Stream_Notification_Rule( $rule_id );
-			$rule_output = array();
-			$triggers = array();
-			$groups = array();
-			foreach ( $rule->triggers as $trigger ) {
-				if ( $trigger['group'] == 0 ) {
-					$triggers[] = $trigger;
-				}
-				if ( isset( $groups[ $trigger['group'] ] ) ) {
-					$groups[ $trigger['group'] ][] = $trigger;
-				} else {
-					$group = $rule->groups[ $trigger['group'] ];
-					$group_chain = array();
-					while ( ! is_null( $rule->groups[$group['group']] ) != null ) {
+			
+			// Generate an easy-to-parse tree of triggers/groups
+			$triggers = $this->generate_tree(
+				$this->generate_flattened_tree(
+					$rule->triggers,
+					$rule->groups
+				)
+			);
 
-					}
-					// while ( $group )
-				}
-			}
+			// Chunkify! @see generate_group_chunks
+			$output[ $rule_id ]['triggers'] = $this->generate_group_chunks(
+				$triggers[0]['triggers']
+			);
 
-			/*
-			$groups = array();//$rule->groups;
-			$groups[2] = array( 'group' => 0, 'relation' => 'or' );
-			$groups[3] = array( 'group' => 2, 'relation' => 'or' );
-			$groups[4] = array( 'group' => 3, 'relation' => 'or' );
-
-			arsort( $groups );
-			$groups[0] = array( 'group' => null, 'relation' => 'and', 'triggers' => array() );
-
-			while ( $group = reset( $groups ) ) {
-				if ( $group['group'] === null ) break;
-				$group['triggers'] = array();
-				$groups[ $group['group'] ]['triggers'][] = $group;
-				unset ( $groups[ key( $groups ) ] );
-			}
-
-			$triggers = array_reverse( $rule->triggers );
-			foreach ( $triggers as $trigger ) {
-				array_unshift( $groups[ $trigger['group'] ]['triggers'], $trigger );
-			}
-
-			{echo '<pre>';var_dump($groups);echo '</pre>';die();}
-			*/
-		
-			// foreach ( $groups as $group )
-			/*
-			$rule_output['trigger_groups'] = array();
-			foreach ( array_values( $rule->triggers ) as $i => $trigger ) {
-				$rule_output['trigger_groups'][ $trigger['group'] ]['triggers'][] = $trigger;
-				$group_relation = ( $trigger['group'] == 0 ) ? 'and' : $groups[ $trigger['group'] ]['relation'];
-				$group_parent   = ( $trigger['group'] == 0 ) ? null : $groups[ $trigger['group'] ]['group'];
-				$rule_output['trigger_groups'][ $trigger['group'] ]['relation'] = $group_relation;
-				if ( ! is_null( $group_parent ) ) {
-					$rule_output['trigger_groups'][ $group_parent ]['subgroups'][$trigger['group']] = $i;
-				}
-			}
-			{echo '<pre>';var_dump($rule_output, $rule_id);echo '</pre>';die();}
-			$rule_output['alerts'] = $rule->alerts;
-			$output[ $rule_id ] = $rule_output;
+			// Add alerts
+			$output[ $rule_id ]['alerts'] = $rule->alerts;
 		}
 		return $output;
-		*/
 	}
 
+	/**
+	 * Return all of group's ancestors starting with the root
+	 */
+	private function generate_group_chain( $groups, $group_id ) {
+		$chain = array();
+		while ( isset( $groups[ $group_id ] ) ) {
+			$chain[] = $group_id;
+			$group_id = $groups[ $group_id ]['group'];
+		}
+		return array_reverse( $chain );
+	}
+	 
+	/**
+	 * Takes the groups and triggers and creates a flattened tree,
+	 * which is an pre-order walkthrough of the tree we want to construct
+	 * http://en.wikipedia.org/wiki/Tree_traversal#Pre-order
+	 */
+	private function generate_flattened_tree( $triggers, $groups ) {
+		// Seed the tree with the universal group
+		if ( ! isset( $groups[0] ) ) {
+			$groups[0] = array( 'group' => null, 'relation' => 'and' );
+		}
+		$flattened_tree      = array( array( 'item' => $groups['0'], 'level' => 0, 'type' => 'group' ) );
+		$current_group_chain = array( '0' );
+		$level               = 1;
+	 
+		foreach ( $triggers as $key => $trigger ) {
+			$active_group = end( $current_group_chain );
+	 
+			// If the trigger goes to any other than actually opened group, we need to traverse the tree first
+			if ( $trigger['group'] != $active_group ) {
+	 
+				$trigger_group_chain   = $this->generate_group_chain( $groups, $trigger['group'] );
+				$common_ancestors      = array_intersect( $current_group_chain, $trigger_group_chain );
+				$newly_inserted_groups = array_diff( $trigger_group_chain, $current_group_chain );
+				$steps_back            = $level - count( $common_ancestors );
+	 
+				// First take the steps back until we reach a common ancestor
+				for ( $i = 0; $i < $steps_back; $i++ ) {
+					array_pop( $current_group_chain );
+					$level--;
+				}
+	 
+				// Then go forward and generate group nodes until the trigger is ready to be inserted
+				foreach ( $newly_inserted_groups as $group ) {
+					$flattened_tree[] = array( 'item' => $groups[ $group ], 'level' => $level++, 'type' => 'group' );
+					$current_group_chain[] = $group;
+				}
+			}
+			// Now we're sure the trigger goes to a correct position
+			$flattened_tree[] = array( 'item' => $trigger, 'level' => $level, 'type' => 'trigger' );
+		}
+	 
+		return $flattened_tree;
+	}
+	 
+	/**
+	 * Takes the flattened tree and generates a proper tree
+	 */
+	private function generate_tree( $flattened_tree ) {
+		// Our recurrent step
+		$recurrent_step = function( $level, $i ) use ( $flattened_tree, &$recurrent_step ) {
+			$return = array();
+			for ( $i; $i < count( $flattened_tree ); $i++ ) {
+				// If we're on the correct level, we're going to insert the node
+				if ( $flattened_tree[$i]['level'] == $level ) {
+					if ( $flattened_tree[$i]['type'] == 'trigger' ) {
+						$return[] = $flattened_tree[$i]['item'];
+						// If the node is a group, we need to call the recursive function
+						// in order to construct the tree for us further
+					} else {
+						$return[] = array(
+							'relation' => $flattened_tree[$i]['item']['relation'],
+							'triggers' => call_user_func( $recurrent_step, $level + 1, $i + 1 ),
+						);
+					}
+					// If we're on a lower level, we came back and we can return this branch
+				} elseif ( $flattened_tree[$i]['level'] < $level ) {
+					return $return;
+				}
+			}
+			return $return;
+		};
+		return call_user_func( $recurrent_step, 0, 0 );
+	}
 
+	/**
+	 * Split trigger trees by relation, so we can fail trigger trees early if
+	 * an effective trigger is not matched
+	 *
+	 * A chunk would be a bulk of triggers that only matches if ANY of its 
+	 * nested triggers are matched
+	 * 
+	 * @param  array  $group Group array, ex: array(
+	 *   'relation' => 'and',
+	 *   'trigger'  => array( arr trigger1, arr trigger2 )
+	 *   );
+	 * @return array         Chunks of triggers, split based on their relation
+	 */
+	private function generate_group_chunks( $triggers ) {
+		$chunks = array();
+		$current_chunk = -1;
+		foreach ( $triggers as $trigger ) {
+			// If is a group, chunks its children as well
+			if ( isset( $trigger['triggers'] ) ) {
+				$trigger['triggers'] = $this->generate_group_chunks( $trigger['triggers'] );
+			}
+			// If relation=and, start a new chunk, else join the previous chunk
+			if ( $trigger['relation'] == 'and' ) {
+				$chunks[] = array( $trigger );
+				$current_chunk = count( $chunks ) - 1;
+			} else {
+				$chunks[ $current_chunk ][] = $trigger;
+			}
+		}
+		return $chunks;
+	}
+
+	private function alert( $rules, $log ) {
+		{echo '<pre>';var_dump( 'alerts via ' . implode( ',', array_keys( $rules ) ) );echo '</pre>';die();}
+	}
 
 }
