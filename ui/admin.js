70629eb51ef410e00876a96f8cc361f4c1e5fe35
johnregan3 <john.regan@x-team.com>
2014-01-27 09:46:50 -0600

    Issue-167 Live updates not respecting screen options

--- ui/admin.js
+++ ui/admin.js
@@ -86,6 +86,13 @@ jQuery(function($){
 			// Add element to the dom
 			$(list_sel).prepend( $new_items );
 
+			$( '.metabox-prefs input' ).each( function() {
+				if( $( this ).prop( 'checked' ) !== true ) {
+					var label = $( this ).val();
+					$( 'td.column-' + label ).hide();
+				}
+			});
+
             // Remove the number of element added to the end of the list table
             $( list_sel + ' tr').slice(-$new_items.length).remove();
 
