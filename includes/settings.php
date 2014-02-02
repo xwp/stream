33f08e589ed9eb34977ae6129875a272ead7079f
Shady Sharaf <shady@sharaf.me>
2014-02-03 00:39:09 +0200

    Added a value to the extra, empty added array element
    So anyone using the values won't find an empty value which can possibly match with anything and cause a severe security flaw, like for example matching user roles, a user with an empty role can pass as valid.
    Resolves #196

--- includes/settings.php
+++ includes/settings.php
@@ -284,7 +284,7 @@ class WP_Stream_Settings {
 				);
 				// Fallback if nothing is selected
 				$output .= sprintf(
-					'<input type="hidden" name="%1$s[%2$s_%3$s][]" />',
+					'<input type="hidden" name="%1$s[%2$s_%3$s][]" value="__placeholder__" />',
 					esc_attr( self::KEY ),
 					esc_attr( $section ),
 					esc_attr( $name )
