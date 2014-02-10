569a0721e9dece23b458f3ed43bbce804f038df0
Frankie Jarrett <frankie@x-team.com>
2014-02-10 16:36:05 -0600

    Method should be static to adhere to PHP strict

--- includes/list-table.php
+++ includes/list-table.php
@@ -587,7 +587,7 @@ class WP_Stream_Notifications_List_Table extends WP_List_Table {
 	/**
 	 * @filter stream_query_args
 	 */
-	function register_occurrences_for_sorting( $args ) {
+	static function register_occurrences_for_sorting( $args ) {
 		if ( $args['orderby'] === 'occurrences' ) {
 			$args['meta_key'] = $args['orderby'];
 			$args['orderby']  = 'meta_value_num';
