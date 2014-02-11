278cdbd10fbd441a1b112fe96ca698c5a444d780
Gustavo Bordoni <webord.net@gmail.com>
2014-02-11 09:39:12 -0200

    We should have a custom page for errors with views, to prevent future mistakes leading to empty pages... #11

---
+++ views/error.php
@@ -0,0 +1,6 @@
+<div class='wrap about-wrap'>
+    <h1><?php _e( "Oops, that's an Error!", 'stream-reports' ); ?></h1>
+    <div class='about-text'>
+        <?php echo wpautop(__( "Yeah... Something went very wrong somewhere along your last actions.", 'stream-reports' )); ?>
+    </div>
+</div>
