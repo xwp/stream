44ee1a87fa0b3d5c964b23098ba99abef4034739
Jonathan Bardo <jonathan.bardo@x-team.com>
2014-02-11 15:04:56 -0800

    Modify html in error view

--- views/error.php
+++ views/error.php
@@ -1,6 +1,6 @@
 <div class='wrap about-wrap'>
     <h1><?php _e( "Oops, that's an Error!", 'stream-reports' ); ?></h1>
     <div class='about-text'>
-        <?php echo wpautop(__( "Yeah... Something went very wrong somewhere along your last actions.", 'stream-reports' )); ?>
+        <?php esc_html_e( 'Yeah... Something went very wrong somewhere along your last actions.', 'stream-reports' ); ?>
     </div>
 </div>
