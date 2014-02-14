9aec95d329627a0ed6f11d4cdc425f09d9c45c16
Gustavo Bordoni <webord.net@gmail.com>
2014-02-14 01:33:34 -0200

    Remove exemple from the the main view

--- views/all.php
+++ views/all.php
@@ -1,26 +1,4 @@
 <div class="wrap">
 	<h2><?php esc_html_e( 'Stream Reports', 'stream-reports' ); ?></h2>
 
-	<div id="chart"></div>
-
-	<script>
-		var chart = c3.generate({
-			data: {
-				columns: [
-					['data1', 30, 200, 100, 400, 150, 250],
-					['data2', 130, 100, 140, 200, 150, 50]
-				],
-				type: 'bar',
-				groups: [
-					['data1', 'data2']
-				]
-			},
-			axis: {
-				x: {
-					type: 'categorized'
-				}
-			}
-		});
-	</script>
-
 </div>
