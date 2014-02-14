388b0f5a5c859bd687ddd001bcd9d764c746e442
Gustavo Bordoni <webord.net@gmail.com>
2014-02-14 07:47:05 -0200

    Using 2 similar Charts to Work on some of the problems I'm facing right now with C3

--- views/exemple-c3.php
+++ views/exemple-c3.php
@@ -1,26 +1,51 @@
 <div class="wrap">
 	<h2><?php esc_html_e( 'C3 Exemple', 'stream-reports' ); ?></h2>
-
-	<div id="chart" style='margin: 15px 0;'></div>
-
-	<script>
-		var chart = c3.generate({
-			data: {
-				columns: [
-					['data1', 30, 200, 100, 400, 150, 250],
-					['data2', 130, 100, 140, 200, 150, 50]
-				],
-				type: 'bar',
-				groups: [
-					['data1', 'data2']
-				]
-			},
-			axis: {
-				x: {
-					type: 'categorized'
-				}
-			}
-		});
-	</script>
-
+	<?php
+		$args = array(
+			'data' => array(
+				'columns' => array(
+					array( 'group', 30, 200, 100, 400, 150, 250 ),
+					array( 'group2', 130, 100, 140, 200, 150, 50 ),
+				),
+				'type' => 'bar',
+				'groups' => array(
+					array( 'group', 'group2' ),
+				),
+				'names' => array(
+					'group' => 'Developmentdasdadad',
+					'group2' => 'Development',
+				),
+			),
+			'axis' => array(
+				'x' => array(
+					'type' => 'categorized',
+				),
+			),
+		);
+	?>
+	<div class="report_chart" style='margin: 15px 0 25px;' data-report-chart='<?php echo json_encode( $args ); ?>'></div>
+	<?php
+		$args = array(
+			'data' => array(
+				'columns' => array(
+					array( 'group', 30, 200, 100, 400, 150, 250 ),
+					array( 'group2', 130, 100, 140, 200, 150, 50 ),
+				),
+				'type' => 'bar',
+				'groups' => array(
+					array( 'group', 'group2' ),
+				),
+				'names' => array(
+					'group' => 'Development',
+					'group2' => 'Development',
+				),
+			),
+			'axis' => array(
+				'x' => array(
+					'type' => 'categorized',
+				),
+			),
+		);
+	?>
+	<div class="report_chart" style='margin: 15px 0 25px;' data-report-chart='<?php echo json_encode( $args ); ?>'></div>
 </div>
