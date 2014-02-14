a7773e196a8e60d75e2ada7fd087bc48ae97dfd9
Gustavo Bordoni <webord.net@gmail.com>
2014-02-14 01:30:36 -0200

    Error page fix the indentation

--- views/error.php
+++ views/error.php
@@ -1,8 +1,8 @@
 <div class='wrap about-wrap'>
-    <h2><?php _e( "Oops, that's an Error!", 'stream-reports' ); ?></h2>
-    <div class='about-text'>
-        <p>
-					<?php esc_html_e( 'Yeah... Something went very wrong somewhere along your last actions.', 'stream-reports' ); ?>
-				</p>
-    </div>
+	<h2><?php _e( "Oops, that's an Error!", 'stream-reports' ); ?></h2>
+	<div class='about-text'>
+		<p>
+			<?php esc_html_e( 'Yeah... Something went very wrong somewhere along your last actions.', 'stream-reports' ); ?>
+		</p>
+	</div>
 </div>
