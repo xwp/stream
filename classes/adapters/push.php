2cf3d1d711d3a85ea32c912070f3771cc2f49367
Krzysztof Powelski <kris@x-team.com>
2014-02-15 18:02:03 +0100

    [issue-67] Add `users` field

--- classes/adapters/push.php
+++ classes/adapters/push.php
@@ -8,7 +8,14 @@ class WP_Stream_Notification_Adapter_Push extends WP_Stream_Notification_Adapter
 
 	public static function fields() {
 		return array(
-			// to be added...
+			'users' => array(
+				'title'    => __( 'Send to Users', 'stream-notifications' ),
+				'type'     => 'hidden',
+				'multiple' => true,
+				'ajax'     => true,
+				'key'      => 'author',
+				'hint'     => __( 'Alert specific users via push.', 'stream-notifications' ),
+			),
 		);
 	}
 
