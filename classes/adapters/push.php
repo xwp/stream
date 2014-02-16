8a2ecc47ca2c3a0d3174a06f45486dda823e2f7c
Krzysztof Powelski <kris@x-team.com>
2014-02-16 13:56:55 +0100

    Add message field if no app key provided

--- classes/adapters/push.php
+++ classes/adapters/push.php
@@ -7,29 +7,43 @@ class WP_Stream_Notification_Adapter_Push extends WP_Stream_Notification_Adapter
 	}
 
 	public static function fields() {
-		return array(
-			'users' => array(
-				'title'    => __( 'Send to Users', 'stream-notifications' ),
-				'type'     => 'hidden',
-				'multiple' => true,
-				'ajax'     => true,
-				'key'      => 'author',
-				'args'     => array(
-					'push' => true,
+		$options = get_option( 'ckpn_pushover_notifications_settings', array() );
+
+		if( isset( $options['application_key'] ) && $options['application_key'] !== '' ) {
+			$fields = array(
+				'users' => array(
+					'title'    => __( 'Send to Users', 'stream-notifications' ),
+					'type'     => 'hidden',
+					'multiple' => true,
+					'ajax'     => true,
+					'key'      => 'author',
+					'args'     => array(
+						'push' => true,
+					),
+					'hint'     => __( 'Alert specific users via push.', 'stream-notifications' ),
 				),
-				'hint'     => __( 'Alert specific users via push.', 'stream-notifications' ),
-			),
-			'subject' => array(
-				'title' => __( 'Subject', 'stream-notifications' ),
-				'type'  => 'text',
-				'hint'  => __( 'Data tags are allowed.', 'stream-notifications' ),
-			),
-			'message' => array(
-				'title' => __( 'Message', 'stream-notifications' ),
-				'type'  => 'textarea',
-				'hint'  => __( 'Data tags are allowed.', 'stream-notifications' ),
-			),
-		);
+				'subject' => array(
+					'title' => __( 'Subject', 'stream-notifications' ),
+					'type'  => 'text',
+					'hint'  => __( 'Data tags are allowed.', 'stream-notifications' ),
+				),
+				'message' => array(
+					'title' => __( 'Message', 'stream-notifications' ),
+					'type'  => 'textarea',
+					'hint'  => __( 'Data tags are allowed.', 'stream-notifications' ),
+				),
+			);
+		} else {
+			$fields = array(
+				'error' => array(
+					'title'   => __( 'Application key is missing', 'stream-notifications' ),
+					'type'    => 'error',
+					'message' => sprintf( __( 'Please provide your Application key on %1$s.', 'stream-notifications' ), sprintf( '<a href="%1$s">%2$s</a>', admin_url( 'options-general.php?page=pushover-notifications' ), __( 'Pushover Notifications settings page', 'stream-notifications' ) ) ),
+				),
+			);
+		}
+
+		return $fields;
 	}
 
 	public function send( $log ) {
