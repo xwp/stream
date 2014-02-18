484987eaa27b2f4bf50ff234bef1b7d5824239b9
Frankie Jarrett <frankie@x-team.com>
2014-02-18 11:42:09 -0600

    4th column not needed

--- views/all.php
+++ views/all.php
@@ -17,9 +17,6 @@
 		<div id="postbox-container-3" class="postbox-container">
 			<?php do_meta_boxes( WP_Stream_Reports::$screen_id, 'column3', '' ); ?>
 		</div>
-		<div id="postbox-container-4" class="postbox-container">
-			<?php do_meta_boxes( WP_Stream_Reports::$screen_id, 'column4', '' ); ?>
-		</div>
 	</div>
 
 </div>
