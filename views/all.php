87ba004f4a48c2a63d05f17ffd0c086004381675
Frankie Jarrett <frankie@x-team.com>
2014-02-18 13:44:15 -0600

    Removing new line that was causing extra space between icon and title

--- views/all.php
+++ views/all.php
@@ -1,18 +1,17 @@
 <div class="wrap">
-	<h2>
-	<?php _e( 'Stream Reports', 'stream-reports' ) ?>
-	<form action="<?php echo admin_url( 'admin-ajax.php' ); //xss ok ?>" method="post" id="stream-report-form">
-		<a href="javascript:void(0)"
-			 id="stream-reports-add-section"
-			 class="add-new-h2"
-			 onclick="document.getElementById('stream-report-form').submit();">
-			<?php esc_html_e( 'Add New Section', 'stream-reports' ); ?>
-		</a>
-		<input type="hidden" name="action" value="stream_reports_add_metabox">
-		<?php wp_nonce_field( 'stream-reports-page', 'stream_report_nonce', false ); ?>
-		<?php wp_nonce_field( 'closedpostboxes', 'closedpostboxesnonce', false ); ?>
-		<?php wp_nonce_field( 'meta-box-order', 'meta-box-order-nonce', false ); ?>
-	</form>
+	<h2><?php _e( 'Stream Reports', 'stream-reports' ) ?>
+		<form action="<?php echo admin_url( 'admin-ajax.php' ); //xss ok ?>" method="post" id="stream-report-form">
+			<a href="javascript:void(0)"
+				 id="stream-reports-add-section"
+				 class="add-new-h2"
+				 onclick="document.getElementById('stream-report-form').submit();">
+				<?php esc_html_e( 'Add New Section', 'stream-reports' ); ?>
+			</a>
+			<input type="hidden" name="action" value="stream_reports_add_metabox">
+			<?php wp_nonce_field( 'stream-reports-page', 'stream_report_nonce', false ); ?>
+			<?php wp_nonce_field( 'closedpostboxes', 'closedpostboxesnonce', false ); ?>
+			<?php wp_nonce_field( 'meta-box-order', 'meta-box-order-nonce', false ); ?>
+		</form>
 	</h2>
 
 	<div id="dashboard-widgets" class="<?php echo esc_attr( $class ) ?>">
