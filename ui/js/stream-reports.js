a1a21bec53cad01293945ecb1b8de14e4afb66b2
Gustavo Bordoni <webord.net@gmail.com>
2014-02-21 09:38:17 -0300

    Working on using NVD3 to draw the charts

--- ui/js/stream-reports.js
+++ ui/js/stream-reports.js
@@ -1,153 +1,100 @@
 /* global Raphael, jQuery, _ */
 (function( window, $, _, Raphael ){
 	'use strict';
-	$.fn.streamReportChart = function (){
-		if ( typeof arguments[0] !== 'undefined' && typeof arguments[0] !== 'object' ){
-			return console.error( 'Error: %s (%s)', 'bad type', typeof arguments[0] );
+
+
+	var report = function(){
+		// Grab all the opts and draw the chart on the screen
+		this.draw = function () {
+
 		}
 
-		var default_args = _.extend(
-				// Default Arguments that will be passed for each element found in `this`
-				// then this will be replace if the functions finds a data attr
-				{
-					width: 'this',
-					height: 'this',
-					animate: true,
-					gutter: 5,
-					type: null,
-					values: [],
+		// Build all the opts to be drawn later
+		this.build = function ( opts ) {
+			if ( _.isArray( opts ) ){
+				opts = _.extend( {}, this._.opts, { '$' : opts.pop() }, ( opts.length > 0 ? opts.pop() : {} ) );
+			}
 
-					// Check if a graph need to be draw
-					draw: true,
+			if ( _.isObject( opts ) ){
+				opts = _.extend( {}, this._.opts, opts );
+			} else {
+				return console.error(  );
+			}
 
-					// Ref to Chart instance
-					chart: null,
-
-					// Ref to Raphael instance
-					paper: null
-				},
-				arguments[0]
-			),
-			_base_ueid = '__stream-report-chart-';
-
-		return this.each(function ( key, el ){
-			var	$el = $(el).on({
-				'build.report': function ( e, data ){
-					if ( _.isObject( $el.data( 'stream-report' ) ) && _.isString( $el.data( 'stream-report' ).id ) ){
-						data = $el.data( 'stream-report' );
-					} else {
-						// Merge the default data, attributes and the param for the init
-						// This is only to give us full control...
-						data = $el.data( 'stream-report', _.extend( {}, default_args, $el.data( 'stream-report' ), data, { 'id': _.uniqueId(_base_ueid) } ) ).data( 'stream-report' );
-						$el.attr( 'id', data.id );
-					}
-
-					if ( 'parent' === data.width || 'parent' === data._width ) {
-						data.width = $el.parent().innerWidth();
-						data._width = 'parent';
-					} else if ( 'this' === data.width || 'this' === data._width ) {
-						data.width = $el.innerWidth();
-						data._width = 'this';
-					}
-
-					if ( 'parent' === data.height || 'parent' === data._height ) {
-						data.height = $el.parent().innerHeight();
-						data._height = 'parent';
-					} else if ( 'this' === data.height || 'this' === data._height ) {
-						data.height = $el.innerHeight();
-						data._height = 'this';
-					}
+			opts.$.each(function( k, el ) {
+				var $el = $(el),
+					data = $el.data( 'report', _.extend( {}, opts, { 'id': _.uniqueId( '__stream-report-chart-' ) }, $el.data( 'report' ) ) ).data( 'report' );
 
 
-					if ( null === data.paper ){
-						data.paper = Raphael( $el[0], data.width, data.height );
-					} else {
-						if ( data.paper.width !== data.width || data.paper.height !== data.height ){
-							data.paper.setSize( data.width, data.height );
-							data.draw = true;
-						}
-					}
+				if ( 'parent' === data.width || 'parent' === data._width ) {
+					data.width = $el.parent().innerWidth();
+					data._width = 'parent';
+				} else if ( 'this' === data.width || 'this' === data._width ) {
+					data.width = $el.innerWidth();
+					data._width = 'this';
+				}
 
-					if ( 'pie' === data.type ) {
-						data.x = data.width/2;
-						data.y = data.height/2;
-						data.radius = Math.ceil( ( _.min( [ data.width, data.height ] ) - data.gutter )/2 );
-					}
-
-					$el.data( 'stream-report', data );
-
-					if ( true === data.draw ){
-						$el.trigger('draw.report');
-					}
-				},
-				'draw.report': function ( e ) {
-					if ( ! _.isObject( $el.data( 'stream-report' ) ) || ! _.isString( $el.data( 'stream-report' ).id ) ){
-						return console.error( 'Error: %s', 'not built yet' );
-					}
-					var data = $el.data( 'stream-report' );
-
-					if ( true === data.draw ){
-						data.paper.clear();
-					}
-
-					// To avoid problems with re-drawing the chart, dont use a global variable
-					// each re-draw build a new values variable
-					var values = [];
-					_.each(data.values, function ( item, key, list ){
-						if ( _.isObject( item ) ){
-							values.push(item.value);
-						} else {
-							values.push(item);
-						}
-					});
-
-					if ( 'pie' === data.type ) {
-						if ( true === data.draw ) {
-							data.chart = data.paper.piechart( data.x, data.y, data.radius, values );
-							data.draw = false;
-						}
-
-						// Animate the chart when it inits
-						if ( true === data.animate && null !== data.chart ) {
-							data.chart.each(function(){
-								this.sector.scale(0, 0, this.cx, this.cy);
-								this.sector.animate({ transform: 's1 1 ' + this.cx + ' ' + this.cy }, 300, "easeInOut");
-							});
-						}
-					} else if ( 'line' === data.type ){
-
-					} else if ( 'vertical-bar' === data.type ){
-
-					} else if ( 'horizontal-bar' === data.type ){
-
-					} else if ( 'dot' === data.type ){
-
-					}
-
-					if ( null !== data.chart ){
-						data.chart.hover(function (){
-							// MouseIn Effect of each item of the chart should go here.
-						}, function (){
-							// MouseOut Effect of each item of the chart should go here.
-						});
-
-						data.chart.click(function() {
-							// On Click event for each one of the things on the chart
-						});
-					}
-
-					$el.data( 'stream-report', data );
+				if ( 'parent' === data.height || 'parent' === data._height ) {
+					data.height = $el.parent().innerHeight();
+					data._height = 'parent';
+				} else if ( 'this' === data.height || 'this' === data._height ) {
+					data.height = $el.innerHeight();
+					data._height = 'this';
 				}
 			});
 
-			$el.trigger('build.report');
-		});
+		}
+		console.log( report._ );
+
+		// Internals
+		this._ = {
+			// Default Options
+			'opts': {
+				// Store the jQuery elements we are using
+				'$': null,
+
+				// Width of the canvas
+				'width': 'this',
+
+				// Height of the Canvas
+				'height': 'this',
+
+				// Actual data that will be plotted
+				'values': {},
+
+				// Type of the Chart
+				'type': false,
+
+				// Miliseconds on the animation, or false to deactivate
+				'animate': 200,
+
+				// Check if a graph need to be draw
+				'draw': null
+			},
+		};
+
+		// After everything is defined start
+		var args = [];
+		Array.prototype.push.apply( args, arguments );
+
+		// Options storage for this instance
+		this.opts = {};
+
+		if ( args[0] instanceof jQuery ){
+			this.build( args );
+		}
+
+		return this;
 	};
+
+
+
+	window.stream = _.extend( ( ! _.isObject( window.stream ) ? {} : window.stream ), { 'report' : report } );
 
 
 	// Just working on a good exemple
 	$(document).ready(function(){
-		$('.report-chart').streamReportChart();
+		var chart = new stream.report( $('.report-chart') );
 
 		// Delete Action
 		$('.postbox').hover(
--- views/ex-pie-chart.php
+++ views/ex-pie-chart.php
@@ -2,11 +2,11 @@
 	<h2><?php esc_html_e( 'Stream Reports', 'stream-reports' ); ?></h2>
 	<?php
 		$args = array(
-			'type' => 'pie',
+			'type' => 'vertical-bar',
 			'values' => array( 6, 10, 300 ),
 		);
 	?>
-	<div class="report-chart" style='height: 200px; width: 200px; float:left;' data-stream-report='<?php echo json_encode( $args ) ?>'></div>
+	<div class="report-chart" style='height: 200px; width: 200px; float:left;' data-report='<?php echo json_encode( $args ) ?>'></div>
 
 	<?php
 		$args = array(
@@ -14,14 +14,14 @@
 			'values' => array( 5, 200, 300, 50 ),
 		);
 	?>
-	<div class="report-chart" style='height: 200px; width: 200px; float:left;' data-stream-report='<?php echo json_encode( $args ) ?>'></div>
+	<div class="report-chart" style='height: 200px; width: 200px; float:left;' data-report='<?php echo json_encode( $args ) ?>'></div>
 
 	<div style='clear:both'></div>
 	<?php
 		$args = array(
-			'type' => 'pie',
+			'type' => 'line',
 			'values' => array( 5, 200, 300, 22, 1, 50 ),
 		);
 	?>
-	<div class="report-chart" style='height: 200px; width: 400px; background: #ddd' data-stream-report='<?php echo json_encode( $args ) ?>'></div>
+	<div class="report-chart" style='height: 200px; width: 400px; background: #ddd' data-report='<?php echo json_encode( $args ) ?>'></div>
 </div>
