52f5b19167fdf2fd577cfd62f4914af41eb5afa7
Frankie Jarrett <frankie@x-team.com>
2014-02-21 14:06:10 -0600

    More specific message about the first trigger

--- includes/form.php
+++ includes/form.php
@@ -325,7 +325,7 @@ class WP_Stream_Notifications_Form
 		// Localization
 		$args['i18n'] = array(
 			'empty_triggers'        => __( 'You cannot save a rule without any triggers.', 'stream-notifications' ),
-			'invalid_first_trigger' => __( 'You cannot save a rule with an empty trigger.', 'stream-notifications' ),
+			'invalid_first_trigger' => __( 'You cannot save a rule with an empty first trigger.', 'stream-notifications' ),
 			'ajax_error'            => __( 'There was an error submitting your request, please try again.', 'stream-notifications' ),
 			'confirm_reset'         => __( 'Are you sure you want to reset occurrences for this rule? This cannot be undone.', 'stream-notifications' ),
 		);
