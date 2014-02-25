725e203181abf4342f523011bd7b09c6403823a2
Udit Desai <desaiuditd@gmail.com>
2014-02-25 12:11:20 +0530

    Removing RWidget RE-order Fix - Solved in other commit

--- connectors/widgets.php
+++ connectors/widgets.php
@@ -98,8 +98,6 @@ class WP_Stream_Connector_Widgets extends WP_Stream_Connector {
 
 		$widget_id = null;
 		$sidebar   = null;
-		unset( $old['array_version'] );
-		unset( $new['array_version'] );
 
 		if ( $deactivated = array_diff( $new['wp_inactive_widgets'], $old['wp_inactive_widgets'] ) ) {
 			$action    = 'deactivated';
