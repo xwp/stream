da9acd3c9f20b6756cf52c678a0d6c3cdd781464
Tyler Carter <tcarter@appthemes.com>
2014-03-03 13:32:39 -0800

    Use jQuery.on instead of click handler

--- ui/dashboard.js
+++ ui/dashboard.js
@@ -1,21 +1,16 @@
 /* dashboard pagination */
 jQuery(function($){
 
-	var dashboardBind = function(){
-		$( '#dashboard_stream_activity .pagination-links a').click(function(e){
-			e.preventDefault();
-			var data = {
-				'action' : 'stream_activity_dashboard_update',
-				'stream-paged' : $(this).data('page'),
-			};
+	$( '#dashboard_stream_activity').on( 'click', '.pagination-links a', function(e){
+		e.preventDefault();
+		var data = {
+			'action' : 'stream_activity_dashboard_update',
+			'stream-paged' : $(this).data('page'),
+		};
 	
-			$.post( window.ajaxurl, data, function( response ){
-				$( '#dashboard_stream_activity .inside' ).html( response );
-				dashboardBind();
-			} );
+		$.post( window.ajaxurl, data, function( response ){
+			$( '#dashboard_stream_activity .inside' ).html( response );
 		} );
-	};
-
-	dashboardBind();
+	} );
 
 });
