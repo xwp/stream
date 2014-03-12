b04c58067bc11dcc9ccfd1bb226afa8ae0ee0386
Jonathan Bardo <jonathan.bardo@x-team.com>
2014-03-11 17:21:45 -0700

    Is null or is false

--- includes/filters.php
+++ includes/filters.php
@@ -85,8 +85,10 @@ class WP_Stream_Filter_Input {
 		}
 
 		// Polyfill the `default` attribute only, for now.
-		if ( is_array( $options ) && ! empty( $options['options']['default'] ) && false === $var ) {
-			return $options['options']['default'];
+		if ( is_array( $options ) && ! empty( $options['options']['default'] ) ) {
+			if ( false === $var || is_null( $var ) ) {
+				return $options['options']['default'];
+			}
 		}
 
 		return $var;
