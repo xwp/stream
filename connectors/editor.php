a58d93ccc30a75f38150251324490a6aa75db876
Krzysztof Powelski <kris@x-team.com>
2014-03-14 08:53:53 +0100

    [issue-313] Compare file contents

--- connectors/editor.php
+++ connectors/editor.php
@@ -15,7 +15,6 @@ class WP_Stream_Connector_Editor extends WP_Stream_Connector {
 	 * @var array
 	 */
 	public static $actions = array(
-		'admin_init',
 		'admin_footer',
 	);
 
@@ -33,6 +32,7 @@ class WP_Stream_Connector_Editor extends WP_Stream_Connector {
 	 */
 	public static function register() {
 		parent::register();
+		add_action( 'load-theme-editor.php', array( __CLASS__, 'get_edition_data' ) );
 		add_filter( 'wp_redirect', array( __CLASS__, 'log_changes_on_redirect' ) );
 	}
 
@@ -73,7 +73,7 @@ class WP_Stream_Connector_Editor extends WP_Stream_Connector {
 	 * @return bool Whether valid edition request is sent
 	 */
 	public static function is_edition_requested() {
-		if ( 'theme-editor' !== get_current_screen()->id ) {
+		if ( ! is_admin() || 'theme-editor' !== get_current_screen()->id ) {
 			return false;
 		}
 
@@ -96,13 +96,24 @@ class WP_Stream_Connector_Editor extends WP_Stream_Connector {
 		return true;
 	}
 
-	public static function callback_admin_init() {
-		if ( ! self::is_edition_requested() ) {
+	/**
+	 * @action load-theme-editor.php
+	 */
+	public static function get_edition_data() {
+		if( 'POST' !== $_SERVER['REQUEST_METHOD'] ) {
+			return;
+		}
+
+		if( ! isset( $_POST['action'] ) || 'update' !== $_POST['action'] ) {
 			return;
 		}
 
 		$theme_name = ( isset( $_POST['theme'] ) && $_POST['theme'] ? $_POST['theme'] : get_stylesheet() );
 		$theme = wp_get_theme( $theme_name );
+
+		if ( ! $theme->exists() || ( $theme->errors() && 'theme_no_stylesheet' === $theme->errors()->get_error_code() ) ) {
+			return;
+		}
 
 		$allowed_files = $theme->get_files( 'php', 1 );
 		$style_files = $theme->get_files( 'css' );
@@ -119,15 +130,15 @@ class WP_Stream_Connector_Editor extends WP_Stream_Connector {
 		$file_contents_before = file_get_contents( $file_path );
 
 		self::$edited_file = compact(
-			$file_name,
-			$file_path,
-			$file_contents_before,
-			$theme
+			'file_name',
+			'file_path',
+			'file_contents_before',
+			'theme'
 		);
 	}
 
 	public static function callback_admin_footer() {
-		if( is_admin() && 'theme-editor' === get_current_screen()->id ) : ?>
+		if( 'theme-editor' === get_current_screen()->id ) : ?>
 			<script>
 				var $content = jQuery('#newcontent');
 
@@ -141,65 +152,40 @@ class WP_Stream_Connector_Editor extends WP_Stream_Connector {
 	}
 
 	public static function log_changes_on_redirect( $location ) {
-		if ( is_admin() && 'theme-editor' === get_current_screen()->id && 'POST' === $_SERVER['REQUEST_METHOD'] && isset( $_POST['action'] ) && 'update' === $_POST['action'] ) {
+		if( ! empty( self::$edited_file ) ) {
+			$file_contents_after = file_get_contents( self::$edited_file['file_path'] );
 
-			if ( isset( $_POST['theme'] ) && $_POST['theme'] ) {
-				$stylesheet = $_POST['theme'];
-			} else {
-				$stylesheet = get_stylesheet();
-			}
+			if ( $file_contents_after !== self::$edited_file['file_contents_before'] ) {
+				$properties = array(
+					'file_html'  => sprintf(
+						'<a href="%s">%s</a>',
+						esc_attr( admin_url( sprintf(
+							'theme-editor.php?theme=%s&file=%s',
+							self::$edited_file['theme']->get_template(),
+							self::$edited_file['file_name']
+						) ) ),
+						self::$edited_file['file_name']
+					),
+					'theme_html' => sprintf(
+						'<a href="%s">%s</a>',
+						esc_attr( admin_url( sprintf( 'themes.php?theme=%s', self::$edited_file['theme']->get_template() ) ) ),
+						self::$edited_file['theme']
+					),
+					'editor_opening_html' => sprintf( '<a href="%s">', esc_attr( admin_url( 'theme-editor.php' ) ) ),
+					'editor_closing_html' => '</a>',
+					'file'       => self::$edited_file['file_name'],
+					'theme'      => self::$edited_file['theme'],
+					'closing'    => '</a>',
+					'new_value'  => self::$edited_file['file_contents_after'],
+					'old_value'  => self::$edited_file['file_contents_before'],
+				);
 
-			$theme = wp_get_theme( $stylesheet );
-
-			if ( $theme->exists() && ! ($theme->errors() && 'theme_no_stylesheet' == $theme->errors()->get_error_code() ) ) {
-				$allowed_files = $theme->get_files( 'php', 1 );
-				$has_templates = ! empty( $allowed_files );
-				$style_files = $theme->get_files( 'css' );
-				$allowed_files['style.css'] = $style_files['style.css'];
-				$allowed_files += $style_files;
-
-				if ( empty( $_POST['file'] ) ) {
-					$relative_file = 'style.css';
-					$file = $allowed_files['style.css'];
-				} else {
-					$relative_file = $_POST['file'];
-					$file = $theme->get_stylesheet_directory() . '/' . $relative_file;
-				}
-
-				$file_contents = file_get_contents( $file );
-
-				if ( $file_contents !== $_POST['oldcontent'] ) {
-					$properties = array(
-						'file_html'  => sprintf(
-							'<a href="%s">%s</a>',
-							esc_attr( admin_url( sprintf(
-								'theme-editor.php?theme=%s&file=%s',
-								$theme->get_template(),
-								$relative_file
-							) ) ),
-							$relative_file
-						),
-						'theme_html' => sprintf(
-							'<a href="%s">%s</a>',
-							esc_attr( admin_url( sprintf( 'themes.php?theme=%s', $theme->get_template() ) ) ),
-							$theme
-						),
-						'editor_opening_html' => sprintf( '<a href="%s">', esc_attr( admin_url( 'theme-editor.php' ) ) ),
-						'editor_closing_html' => '</a>',
-						'file'       => $relative_file,
-						'theme'      => $theme,
-						'closing'    => '</a>',
-						'new_value'  => $file_contents,
-						'old_value'  => $_POST['oldcontent'],
-					);
-
-					self::log(
-						__( '%1$s file of %2$s theme was updated via %3$sEditor%4$s', 'stream' ),
-						$properties,
-						null,
-						array( 'file' => 'updated' )
-					);
-				}
+				self::log(
+					__( '%1$s file of %2$s theme was updated via %3$sEditor%4$s', 'stream' ),
+					$properties,
+					null,
+					array( 'file' => 'updated' )
+				);
 			}
 		}
 
