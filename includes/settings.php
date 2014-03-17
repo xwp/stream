b79bb8179a5c297dab37783261122cc54c2fbcf1
Krzysztof Powelski <kris@x-team.com>
2014-03-17 11:51:32 +0100

    [issue-335] Add Ajax callback

--- includes/settings.php
+++ includes/settings.php
@@ -64,6 +64,9 @@ class WP_Stream_Settings {
 		// Ajax callback function to search users
 		add_action( 'wp_ajax_stream_get_users', array( __CLASS__, 'get_users' ) );
 
+		// Ajax callback function to search IPs
+		add_action( 'wp_ajax_stream_get_ips', array( __CLASS__, 'get_ips' ) );
+
 	}
 
 	/**
@@ -134,6 +137,27 @@ class WP_Stream_Settings {
 
 			$response->users[] = $args;
 		}
+
+		wp_send_json_success( $response );
+	}
+
+	/**
+	 * Ajax callback function to search IP addresses that is used on exclude setting page
+	 *
+	 * @uses WP_User_Query WordPress User Query class.
+	 * @return void
+	 */
+	public static function get_ips(){
+		if ( ! defined( 'DOING_AJAX' ) ) {
+			return;
+		}
+		if ( ! current_user_can( WP_Stream_Admin::SETTINGS_CAP ) ) {
+			return;
+		}
+
+		check_ajax_referer( 'stream_get_ips', 'nonce' );
+
+		$response = array();
 
 		wp_send_json_success( $response );
 	}
@@ -260,6 +284,7 @@ class WP_Stream_Settings {
 							'desc'        => __( 'No activity will be logged for these IP addresses.', 'stream' ),
 							'class'       => 'ip-addresses',
 							'default'     => array(),
+							'nonce'       => 'stream_get_ips',
 						),
 					),
 				),
@@ -371,6 +396,7 @@ class WP_Stream_Settings {
 		$href          = isset( $field['href'] ) ? $field['href'] : null;
 		$after_field   = isset( $field['after_field'] ) ? $field['after_field'] : null;
 		$title         = isset( $field['title'] ) ? $field['title'] : null;
+		$nonce         = isset( $field['nonce'] ) ? $field['nonce'] : null;
 		$current_value = self::$options[ $section . '_' . $name ];
 
 		if ( is_callable( $current_value ) ) {
@@ -506,13 +532,14 @@ class WP_Stream_Settings {
 					esc_attr( $name )
 				);
 				$output .= sprintf(
-					'<input type="hidden" data-values=\'%1$s\' data-selected=\'%2$s\' value="%3$s" class="select2-select %4$s" data-select-placeholder="%5$s-%6$s-select-placeholder"  />',
+					'<input type="hidden" data-values=\'%1$s\' data-selected=\'%2$s\' value="%3$s" class="select2-select %4$s" data-select-placeholder="%5$s-%6$s-select-placeholder" %7$s />',
 					esc_attr( json_encode( $data_values ) ),
 					esc_attr( json_encode( $selected_values ) ),
 					esc_attr( implode( ',', $current_value ) ),
 					$class,
 					esc_attr( $section ),
-					esc_attr( $name )
+					esc_attr( $name ),
+					isset( $nonce ) ? sprintf( ' data-nonce="%s"', esc_attr( wp_create_nonce( $nonce ) ) ) : ''
 				);
 				// to store data with default value if nothing is selected
 				$output .= sprintf(
