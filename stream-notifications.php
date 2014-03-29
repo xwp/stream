2ff93f201f2a134821884fb97e2919c34a4930a4
Frankie Jarrett <frankie@x-team.com>
2014-03-28 22:39:48 -0500

    Use semver compat version number

--- stream-notifications.php
+++ stream-notifications.php
@@ -4,7 +4,7 @@
  * Depends: Stream
  * Plugin URI: http://x-team.com
  * Description: TBD
- * Version: 0.1
+ * Version: 0.1.0
  * Author: X-Team
  * Author URI: http://x-team.com/wordpress/
  * License: GPLv2+
@@ -22,15 +22,13 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Founda
- * tion, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
-
 
 class WP_Stream_Notifications {
 
