76a20e5f96cc1d928f0eabf6646dad14a8df8a2d
Luke Carbis <luke@carbis.com.au>
2014-04-01 11:28:59 +1000

    Replace stream_test_ with wp_stream_test_

--- includes/deprecated.php
+++ includes/deprecated.php
@@ -3,12 +3,12 @@
 /**
  * stream_query()
  *
- * @deprecated 1.2.4
+ * @deprecated 1.3.1
  * @deprecated Use wp_stream_query()
  * @see wp_stream_query()
  */
 function stream_query( $args = array() ) {
-	_deprecated_function( __FUNCTION__, '1.2.4', 'wp_stream_query()' );
+	_deprecated_function( __FUNCTION__, '1.3.1', 'wp_stream_query()' );
 
 	return wp_stream_query( $args );
 }
@@ -16,12 +16,12 @@ function stream_query( $args = array() ) {
 /**
  * get_stream_meta()
  *
- * @deprecated 1.2.4
+ * @deprecated 1.3.1
  * @deprecated Use wp_stream_get_meta
  * @see wp_stream_get_meta()
  */
 function get_stream_meta( $record_id, $key = '', $single = false ) {
-	_deprecated_function( __FUNCTION__, '1.2.4', 'wp_stream_get_meta()' );
+	_deprecated_function( __FUNCTION__, '1.3.1', 'wp_stream_get_meta()' );
 
 	return wp_stream_get_meta( $record_id, $key, $single );
 }
@@ -29,12 +29,12 @@ function get_stream_meta( $record_id, $key = '', $single = false ) {
 /**
  * update_stream_meta()
  *
- * @deprecated 1.2.4
+ * @deprecated 1.3.1
  * @deprecated Use wp_stream_update_meta
  * @see wp_stream_update_meta()
  */
 function update_stream_meta( $record_id, $meta_key, $meta_value, $prev_value = '' ) {
-	_deprecated_function( __FUNCTION__, '1.2.4', 'wp_stream_update_meta()' );
+	_deprecated_function( __FUNCTION__, '1.3.1', 'wp_stream_update_meta()' );
 
 	return wp_stream_update_meta( $record_id, $meta_key, $meta_value, $prev_value );
 }
@@ -42,12 +42,12 @@ function update_stream_meta( $record_id, $meta_key, $meta_value, $prev_value = '
 /**
  * existing_records()
  *
- * @deprecated 1.2.4
+ * @deprecated 1.3.1
  * @deprecated Use wp_stream_existing_records
  * @see wp_stream_existing_records()
  */
 function existing_records( $column, $table = '' ) {
-	_deprecated_function( __FUNCTION__, '1.2.4', 'wp_stream_existing_records()' );
+	_deprecated_function( __FUNCTION__, '1.3.1', 'wp_stream_existing_records()' );
 
 	return wp_stream_existing_records( $column, $table );
 }
