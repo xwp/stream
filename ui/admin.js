a53d8f00a87575658b2b537f1134ee2f040ee5f9
Frankie Jarrett <frankie@x-team.com>
2014-03-31 22:15:43 -0500

    Don't auto-focus

--- ui/admin.js
+++ ui/admin.js
@@ -487,7 +487,6 @@ jQuery(function($){
 
 						if ('custom' === value) {
 							dateinputs.show();
-							dateinputs.find('.field-from').focus();
 							return false;
 						} else {
 							dateinputs.hide();
