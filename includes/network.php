d15672f1eef4c2a8cdf5548b3083322017007292
Luke Carbis <luke.carbis@x-team.com>
2014-04-15 23:46:48 +1000

    Replace deprecated filter

--- includes/network.php
+++ includes/network.php
@@ -34,7 +34,7 @@ class WP_Stream_Network {
 		add_filter( 'stream_toggle_filters', array( $this, 'toggle_filters' ) );
 		add_filter( 'wp_stream_db_tables_prefix', array( $this, 'db_tables_prefix' ) );
 		add_filter( 'wp_stream_list_table_screen_id', array( $this, 'list_table_screen_id' ) );
-		add_filter( 'stream_query_args', array( $this, 'set_network_option_value' ) );
+		add_filter( 'wp_stream_query_args', array( $this, 'set_network_option_value' ) );
 		add_filter( 'wp_stream_list_table_columns', array( $this, 'network_admin_columns' ) );
 	}
 
