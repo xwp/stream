ddfe2f9c1b8c87c053125986f668db5b9e9632c5
Frankie Jarrett <frankie@x-team.com>
2014-04-16 20:18:03 -0500

    Add site count to Reports screen title

--- views/all.php
+++ views/all.php
@@ -1,10 +1,28 @@
 <div class="wrap">
 
-	<h2><?php esc_html_e( 'Stream Reports', 'stream-reports' ) ?>
-		<a href="<?php echo esc_url( $add_url ) ?>" class="add-new-h2">
-			<?php esc_html_e( 'Add New', 'stream-reports' ) ?>
-		</a>
-	</h2>
+	<?php if ( is_network_admin() ) : ?>
+
+		<?php
+		$site_count = sprintf( _n( '1 site', '%d sites', get_blog_count(), 'stream-reports' ), get_blog_count() );
+
+		printf(
+			'<h2>%s (%s)<a href="%s" class="add-new-h2">%s</a></h2>',
+			esc_html__( 'Stream Reports', 'stream-reports' ),
+			$site_count, // xss ok
+			esc_url( $add_url ),
+			esc_html__( 'Add New', 'stream-reports' )
+		);
+		?>
+
+	<?php else : ?>
+
+		<h2><?php esc_html_e( 'Stream Reports', 'stream-reports' ) ?>
+			<a href="<?php echo esc_url( $add_url ) ?>" class="add-new-h2">
+				<?php esc_html_e( 'Add New', 'stream-reports' ) ?>
+			</a>
+		</h2>
+
+	<?php endif; ?>
 
 	<?php wp_nonce_field( 'stream-reports-page', 'wp_stream_reports_nonce', false ) ?>
 	<?php wp_nonce_field( 'closedpostboxes', 'closedpostboxesnonce', false ) ?>
