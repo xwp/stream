9950cab10ab395e67fd0967ef3cc0abd70ed6fac
Chris Olbekson <chris.olbekson@x-team.com>
2014-04-22 15:36:40 -0500

    Adding extensions.js file

---
+++ ui/extensions.js
@@ -0,0 +1,32 @@
+/**
+ * global jQuery, ajaxurl, tb_remove, alert, stream_activation, tb_show, alert, console
+ */
+
+(function($) {
+
+	var Extensions = {
+
+		init : function() {
+			this.load();
+		},
+
+		load : function() {
+			var template = $('.theme-wrap');
+			$('.theme').on('click', function() {
+				var $this = $(this);
+				var extension = $this.data('extension');
+				$.each(stream_extensions.extensions, function(index, value) {
+					if (extension === index) {
+						console.log(value);
+						template.html('<img src="'+ value.screen_shot + '" />' + value.description);
+					}
+				});
+			});
+		}
+	};
+
+	$(document).ready(function() {
+		Extensions.init();
+	});
+
+})(jQuery);
