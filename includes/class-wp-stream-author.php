767f7b27270ba01619b1ada3c552c5f207bc39ce
Frankie Jarrett <frankie@x-team.com>
2014-05-10 16:50:50 -0400

    Remove assert

--- includes/class-wp-stream-author.php
+++ includes/class-wp-stream-author.php
@@ -120,10 +120,13 @@ class WP_Stream_Author {
 	 */
 	function get_avatar_src( $size = 80 ) {
 		$img = $this->get_avatar_img( $size );
+
 		if ( ! $img ) {
 			return false;
 		}
-		assert( preg_match( '/src=([\'"])(.*?)\1/', $img, $matches ) );
+
+		preg_match( '/src=([\'"])(.*?)\1/', $img, $matches );
+
 		$src = html_entity_decode( $matches[2] );
 
 		return $src;
