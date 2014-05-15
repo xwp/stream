fc96da14e75f03161cd04eb1339a37be83a1954f
Frankie Jarrett <frankie@x-team.com>
2014-05-15 13:35:17 -0500

    Remove superfluous install call, handled by verify_database_present earlier

--- stream.php
+++ stream.php
@@ -112,11 +112,10 @@ class WP_Stream {
 
 		if ( is_admin() ) {
 			require_once WP_STREAM_INC_DIR . 'admin.php';
-			require_once WP_STREAM_INC_DIR . 'extensions.php';
 			add_action( 'plugins_loaded', array( 'WP_Stream_Admin', 'load' ) );
-			add_action( 'admin_init', array( 'WP_Stream_Extensions', 'get_instance' ) );
 
-			add_action( 'init', array( __CLASS__, 'install' ), 10, 1 );
+			require_once WP_STREAM_INC_DIR . 'extensions.php';
+			add_action( 'admin_init', array( 'WP_Stream_Extensions', 'get_instance' ) );
 
 			// Registers a hook that connectors and other plugins can use whenever a stream update happens
 			add_action( 'admin_init', array( __CLASS__, 'update_activation_hook' ) );
