4ecb17d343a2e3f8dfecc0fa2401605cca9b726e
Frankie Jarrett <frankie@x-team.com>
2014-05-20 14:07:13 -0500

    Use singlequotes in strings

--- ui/admin.js
+++ ui/admin.js
@@ -780,6 +780,6 @@ jQuery(function($){
 
 jQuery.extend({
 	getQueryParameters : function( str ) {
-		return (str || document.location.search).replace(/(^\?)/,'').split("&").map(function(n){return n = n.split("="),this[n[0]] = n[1],this}.bind({}))[0];
+		return (str || document.location.search).replace(/(^\?)/,'').split('&').map(function(n){return n = n.split('='),this[n[0]] = n[1],this}.bind({}))[0];
 	}
 });
