74fbb419bfa29a73a3195547dff601fefea8f7f6
Tyler Carter <tyler@tylercarter.org>
2014-06-06 10:05:20 -0700

    Separate chart creeating code into its own class

--- includes/meta-boxes.php
+++ includes/meta-boxes.php
@@ -32,15 +32,11 @@ class WP_Stream_Reports_Metaboxes {
 	public function __construct() {
 		// Get all sections from the database
 		self::$sections = WP_Stream_Reports_Settings::get_user_options( 'sections' );
+		$this->charts   = new WP_Stream_Reports_Charts();
 
 		// Load records
 		add_filter( 'wp_stream_reports_load_records', array( $this, 'sort_coordinates_by_count' ), 10, 2 );
 		add_filter( 'wp_stream_reports_load_records', array( $this, 'limit_coordinates' ), 10, 2 );
-
-		// Make charts
-		add_filter( 'wp_stream_reports_make_chart', array( $this, 'pie_chart_coordinates' ), 10, 2 );
-		add_filter( 'wp_stream_reports_make_chart', array( $this, 'bar_chart_coordinates' ), 10, 2 );
-		add_filter( 'wp_stream_reports_make_chart', array( $this, 'line_chart_coordinates' ), 10, 2 );
 
 		// Finalize charts
 		add_filter( 'wp_stream_reports_finalize_chart', array( $this, 'translate_labels' ), 10, 2 );
@@ -287,99 +283,6 @@ class WP_Stream_Reports_Metaboxes {
 		return apply_filters( 'wp_stream_reports_finalize_chart', $coordinates, $args );
 	}
 
-	public function line_chart_coordinates( $records, $args ) {
-		if ( 'line' !== $args['chart_type'] ) {
-			return $records;
-		}
-
-		$sorted = array();
-
-		// Get date count for each sort
-		foreach ( $records as $type => $items ) {
-			$sorted[ $type ] = $this->count_by_field( 'created', $items, array( $this, 'collapse_dates' ) );
-		}
-
-		$sorted = $this->pad_fields( $sorted );
-
-		foreach ( $sorted as $type => &$items ) {
-			ksort( $items );
-		}
-
-		$coordinates = array();
-		foreach ( $sorted as $line_name => $points ) {
-			$line_data = array(
-				'key'    => $line_name,
-				'values' => array(),
-			);
-
-			foreach ( $points as $x => $y ) {
-				$line_data['values'][] = array(
-					'x' => $x,
-					'y' => $y,
-				);
-			}
-
-			$coordinates[] = $line_data;
-		}
-
-		return $coordinates;
-	}
-
-	public function pie_chart_coordinates( $records, $args ) {
-		if ( 'pie' !== $args['chart_type'] ) {
-			return $records;
-		}
-		$counts = array();
-
-		foreach ( $records as $type => $items ) {
-			$counts[] = array(
-				'key'   => $type,
-				'value' => count( $items ),
-			);
-		}
-
-		return $counts;
-	}
-
-	public function bar_chart_coordinates( $records, $args ) {
-		if ( 'multibar' !== $args['chart_type'] ) {
-			return $records;
-		}
-
-		$sorted = array();
-
-		// Get date count for each sort
-		foreach ( $records as $type => $items ) {
-			$sorted[ $type ] = $this->count_by_field( 'created', $items, array( $this, 'collapse_dates' ) );
-		}
-
-		$sorted = $this->pad_fields( $sorted );
-
-		foreach ( $sorted as $type => &$items ) {
-			ksort( $items );
-		}
-
-		$coordinates = array();
-
-		foreach ( $sorted as $line_name => $points ) {
-			$line_data = array(
-				'key'    => $line_name,
-				'values' => array(),
-			);
-
-			foreach ( $points as $x => $y ) {
-				$line_data['values'][] = array(
-					'x' => $x,
-					'y' => $y,
-				);
-			}
-
-			$coordinates[] = $line_data;
-		}
-
-		return $coordinates;
-	}
-
 	public function apply_chart_settings( $coordinates, $args ) {
 		foreach ( $coordinates as $key => $dataset ) {
 			if ( in_array( $key, $args['disabled'] ) ) {
@@ -610,7 +513,7 @@ class WP_Stream_Reports_Metaboxes {
 				}
 			}
 		}
-		$sorted = $this->group_by_field( $grouping_field, $unsorted );
+		$sorted = $this->charts->group_by_field( $grouping_field, $unsorted );
 
 		return $sorted;
 	}
@@ -678,78 +581,7 @@ class WP_Stream_Reports_Metaboxes {
 		$top_elements['report-others'] = $other_element;
 
 		return $top_elements;
-	}
-
-	/**
-	 * Groups objects with similar field properties into arrays
-	 * @return array
-	 */
-	protected function group_by_field( $field, $records, $callback = '' ) {
-		$sorted = array();
-		foreach ( $records as $record ) {
-			$key = $record->$field;
-
-			if ( is_callable( $callback ) ) {
-				$key = call_user_func( $callback, $key );
-			}
-
-			if ( array_key_exists( $key, $sorted ) && is_array( $sorted[ $key ] ) ) {
-				$sorted[ $key ][] = $record;
-			} else {
-				$sorted[ $key ] = array( $record );
-			}
-		}
-
-		return $sorted;
-	}
-
-	/**
-	 * Counts the number of objects with similar field properties in an array
-	 * @return array
-	 */
-	protected function count_by_field( $field, $records, $callback = '' ) {
-		$sorted = $this->group_by_field( $field, $records, $callback );
-		$counts = array();
-
-		foreach ( array_keys( $sorted ) as $key ) {
-			$counts[ $key ] = count( $sorted[ $key ] );
-		}
-
-		return $counts;
-	}
-
-	/**
-	 * Adds blank fields for all keys present in any array
-	 * @return array
-	 */
-	protected function pad_fields( $records ) {
-		$keys = array();
-
-		foreach ( $records as $dataset ) {
-			$keys = array_unique( array_merge( $keys, array_keys( $dataset ) ) );
-		}
-
-		$new_records = array();
-
-		foreach ( $keys as $key ) {
-			foreach ( $records as $data_key => $dataset ) {
-				if ( ! array_key_exists( $data_key, $new_records ) ) {
-					$new_records[ $data_key ] = array();
-				}
-
-				$new_records[ $data_key ][ $key ] = isset( $records[ $data_key ][ $key ] ) ? $records[ $data_key ][ $key ] : 0;
-			}
-		}
-
-		return $new_records;
-	}
-
-	/**
-	 * Used to group data points by day
-	 */
-	protected function collapse_dates( $date ) {
-		return strtotime( date( 'Y-m-d', strtotime( $date ) ) );
-	}
+	}	
 
 	/**
 	 * Creates a title generated from the arguments for the chart
