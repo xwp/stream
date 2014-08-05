a0163f8e43afeb42b80e7fdf67c1441c3bf8391f
Frankie Jarrett <frankie@x-team.com>
2014-08-04 23:19:12 -0500

    Add context table queries into first DB query to improve performance

--- classes/class-wp-stream-legacy-update.php
+++ classes/class-wp-stream-legacy-update.php
@@ -48,10 +48,23 @@ class WP_Stream_Legacy_Update {
 	public static function get_chunk( $offset = 0 ) {
 		global $wpdb;
 
-		$records = $wpdb->get_results( $wpdb->prepare( "SELECT * FROM {$wpdb->prefix}stream WHERE type = 'stream' ORDER BY created ASC LIMIT %d, %d", $offset, self::$limit ), ARRAY_A );
+		$records = $wpdb->get_results(
+			$wpdb->prepare( "
+				SELECT s.*, sc.connector, sc.context, sc.action
+				FROM {$wpdb->prefix}stream AS s, {$wpdb->prefix}stream_context AS sc
+				WHERE s.type = 'stream'
+					AND sc.record_id = s.ID
+				ORDER BY s.created ASC
+				LIMIT %d, %d
+				",
+				$offset,
+				self::$limit
+			),
+			ARRAY_A
+		);
 
 		foreach ( $records as $record => $data ) {
-			$stream_contexts    = $wpdb->get_row( $wpdb->prepare( "SELECT connector, context, action FROM {$wpdb->prefix}stream_context WHERE record_id = %d", $records[ $record ]['ID'] ), ARRAY_A );
+			// @TODO Figure out a way to eliminate this additional query using a JOIN above, if possible
 			$stream_meta        = $wpdb->get_results( $wpdb->prepare( "SELECT meta_key, meta_value FROM {$wpdb->prefix}stream_meta WHERE record_id = %d", $records[ $record ]['ID'] ), ARRAY_A );
 			$stream_meta_output = array();
 
@@ -59,9 +72,6 @@ class WP_Stream_Legacy_Update {
 				$stream_meta_output[ $value['meta_key'] ] = maybe_unserialize( $value['meta_value'] );
 			}
 
-			$records[ $record ]['connector']   = $stream_contexts['connector'];
-			$records[ $record ]['context']     = $stream_contexts['context'];
-			$records[ $record ]['action']      = $stream_contexts['action'];
 			$records[ $record ]['stream_meta'] = $stream_meta_output;
 			$records[ $record ]['created']     = wp_stream_get_iso_8601_extended_date( strtotime( $records[ $record ]['created'] ) );
 
