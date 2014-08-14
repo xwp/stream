beed553dc9b4a6b415661d30398f44c65c29ee4c
JeffMatson <jeff@jeffmatson.net>
2014-08-13 20:42:35 -0400

    Changed "cell provider" to "mobile provider"

--- classes/adapters/sms.php
+++ classes/adapters/sms.php
@@ -18,7 +18,7 @@ class WP_Stream_Notification_Adapter_SMS extends WP_Stream_Notification_Adapter 
 			'carrier' => array(
 				'title'   => __( 'Carrier', 'stream-notifications' ),
 				'type'    => 'select',
-				'hint'    => __( 'Select your cell provider.', 'stream-notifications' ),
+				'hint'    => __( 'Select your mobile provider.', 'stream-notifications' ),
 				'ajax'    => false,
 				'options' => array(
 					'@sms.3rivers.net'          => esc_html__( '3 River Wireless', 'stream-notifications' ),
