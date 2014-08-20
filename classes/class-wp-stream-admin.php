4153057a037c56c4781269ce791d5ec8c1bd8adb
Luke Carbis <luke.carbis@x-team.com>
2014-08-20 14:16:20 +1200

    Remove reference to old cron hook

--- classes/class-wp-stream-admin.php
+++ classes/class-wp-stream-admin.php
@@ -497,7 +497,6 @@ class WP_Stream_Admin {
 		delete_option( WP_Stream_API::SITE_UUID_OPTION_KEY );
 
 		wp_clear_scheduled_hook( WP_Stream_Log::LOG_CLEAN_BUFFER_CRON_HOOK );
-		wp_clear_scheduled_hook( WP_Stream_Log::LOG_INSERT_RECORD_CRON_HOOK );
 	}
 
 	public static function get_testimonials() {
