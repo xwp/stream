832130bc2b0bb776dd23a304d4257b2a5ebb0aea
Luke Carbis <luke.carbis@x-team.com>
2014-08-22 17:59:39 +1200

    Allow sites override even from network admin

--- classes/class-wp-stream-api.php
+++ classes/class-wp-stream-api.php
@@ -229,10 +229,12 @@ class WP_Stream_API {
 	 * @return mixed
 	 */
 	public function search( $query = array(), $fields = array(), $sites = array(), $allow_cache = false, $expiration = 120 ) {
-		if ( is_network_admin() ) {
-			$sites = array_keys( WP_Stream_Network::get_instance()->connected_sites );
-		} elseif ( empty( $sites ) && $this->site_uuid ) {
-			$sites[] = $this->site_uuid;
+		if ( empty( $sites ) ) {
+			if ( is_network_admin() ) {
+				$sites = array_keys( WP_Stream_Network::get_instance()->connected_sites );
+			} elseif ( $this->site_uuid ) {
+				$sites[] = $this->site_uuid;
+			}
 		}
 
 		$url  = $this->request_url( '/search' );
