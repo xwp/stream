0d30d903a0316490a13e89d01633939ca236acbe
Luke Carbis <luke.carbis@x-team.com>
2014-09-04 08:26:46 +1000

    Revert "Add xss ok comment"
    This reverts commit a99b3afd15ffe97e4918dbd5d2f8fc23fa7ed900.

--- includes/migrate.php
+++ includes/migrate.php
@@ -17,7 +17,7 @@ if ( ! $api_key || $api_key !== $api->api_key ) {
 	wp_die( 'Unauthorized.', 'stream' );
 }
 
-$records = json_encode( $migrate->get_records() ); //xss ok
+$records = json_encode( $migrate->get_records() );
 
 header( 'Content-type: application/json' );
 header( 'Content-Length: ' . strlen( $records ) );
