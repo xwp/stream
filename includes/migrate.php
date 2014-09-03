d2858cfb2628bb74a4511e224d94377d093982f0
Luke Carbis <luke.carbis@x-team.com>
2014-09-03 16:10:57 +1000

    Add migrate script

---
+++ includes/migrate.php
@@ -0,0 +1,27 @@
+<?php
+
+// This file is meant to be access directly
+if ( defined( 'ABSPATH' ) ) {
+	wp_die( 'Doing it wrong.', 'stream' );
+}
+
+define( 'WP_USE_THEMES', false );
+require( '../../../../wp-load.php' );
+
+$api     = new WP_Stream_API();
+$migrate = new WP_Stream_Migrate();
+
+$api_key = wp_stream_filter_input( INPUT_GET, 'api_key' );
+
+if ( ! $api_key || $api_key !== $api->api_key ) {
+	wp_die( 'Unauthorized.', 'stream' );
+}
+
+$records = json_encode( $migrate->get_records() );
+
+header( 'Content-type: application/json' );
+header( 'Content-Length: ' . strlen( $records ) );
+
+echo $records;
+
+die();
