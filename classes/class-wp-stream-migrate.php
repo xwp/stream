7d1f22f2929e70370dcb208283ace97bfc6e46b5
Frankie Jarrett <frankie@x-team.com>
2014-09-05 12:31:57 -0500

    Undo last commit

--- classes/class-wp-stream-migrate.php
+++ classes/class-wp-stream-migrate.php
@@ -324,18 +324,18 @@ class WP_Stream_Migrate {
 				unset( $records[ $record ]['parent'] );
 
 				// Ensure required fields always exist
-				$records[ $record ]['site_id']   = ! empty( $records[ $record ]['site_id'] )   ? $records[ $record ]['site_id']   : 1;
-				$records[ $record ]['blog_id']   = ! empty( $records[ $record ]['blog_id'] )   ? $records[ $record ]['blog_id']   : 1;
-				$records[ $record ]['object_id'] = ! empty( $records[ $record ]['object_id'] ) ? $records[ $record ]['object_id'] : 0;
-				$records[ $record ]['author']    = ! empty( $records[ $record ]['author'] )    ? $records[ $record ]['author']    : 0;
+				$records[ $record ]['site_id']     = ! empty( $records[ $record ]['site_id'] )     ? $records[ $record ]['site_id']     : 1;
+				$records[ $record ]['blog_id']     = ! empty( $records[ $record ]['blog_id'] )     ? $records[ $record ]['blog_id']     : 1;
+				$records[ $record ]['object_id']   = ! empty( $records[ $record ]['object_id'] )   ? $records[ $record ]['object_id']   : 0;
+				$records[ $record ]['author']      = ! empty( $records[ $record ]['author'] )      ? $records[ $record ]['author']      : 0;
+				$records[ $record ]['author_role'] = ! empty( $records[ $record ]['author_role'] ) ? $records[ $record ]['author_role'] : '';
 			}
 
-			// Recursive sanitization of values within the record
+			// If the array value is numeric then sanitize it from a string to an int
 			array_walk_recursive(
 				$records[ $record ],
 				function( &$v ) {
 					$v = ctype_digit( $v ) ? intval( $v ) : $v; // If the value is comprised of only digits then make it an int
-					$v = is_null( $v ) ? '' : $v; // Always convert null values to empty strings
 				}
 			);
 		}
