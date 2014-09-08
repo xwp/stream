7330c826a00c4e9d8687f270b7289c885753a02c
Luke Carbis <luke.carbis@x-team.com>
2014-09-09 09:59:45 +1000

    Complexify record count statement again

--- classes/class-wp-stream-migrate.php
+++ classes/class-wp-stream-migrate.php
@@ -61,15 +61,17 @@ class WP_Stream_Migrate {
 			return;
 		}
 
-		self::$site_id      = is_multisite() ? get_current_site()->id : 1;
-		self::$blog_id      = is_network_admin() ? 0 : get_current_blog_id();
+		self::$site_id = is_multisite() ? get_current_site()->id : 1;
+		self::$blog_id = is_network_admin() ? 0 : get_current_blog_id();
+
 		self::$record_count = $wpdb->get_var(
 			$wpdb->prepare( "
 				SELECT COUNT(*)
-				FROM `{$wpdb->base_prefix}stream`
-				WHERE site_id = %d
-					AND blog_id = %d
-					AND type = 'stream'
+				FROM {$wpdb->base_prefix}stream AS s, {$wpdb->base_prefix}stream_context AS sc
+				WHERE s.site_id = %d
+					AND s.blog_id = %d
+					AND s.type = 'stream'
+					AND sc.record_id = s.ID
 				",
 				self::$site_id,
 				self::$blog_id
