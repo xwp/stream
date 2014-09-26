4544f7863ff17f78b5bbbbbb8105494095eb0283
Frankie Jarrett <frankie@x-team.com>
2014-09-26 23:03:02 +1000

    Use esc_html_x where applicable, build love letter URL with add_query_arg

--- classes/class-wp-stream-admin.php
+++ classes/class-wp-stream-admin.php
@@ -883,10 +883,18 @@ class WP_Stream_Admin {
 
 				<div class="stream-haiku">
 					<p>
-						<?php echo esc_html( _x( 'A transformation', 'Haiku', 'stream' ) ) ?><br />
-						<?php echo esc_html( _x( 'Like brook, to river, to sea', 'Haiku', 'stream' ) ) ?><br />
-						<a href="<?php echo esc_html( self::PUBLIC_URL ) ?>/love-letter?site-name=<?php echo urlencode( get_bloginfo( 'name' ) ) ?>" target="_blank">
-							<?php echo esc_html( _x( 'I have a secret', 'Haiku', 'stream' ) ) ?>
+						<?php echo esc_html_x( 'A transformation', 'Haiku line 1', 'stream' ) ?><br />
+						<?php echo esc_html_x( 'Like brook, to river, to sea', 'Haiku line 2', 'stream' ) ?><br />
+						<?php
+						$love_letter_url = add_query_arg(
+							array(
+								'site-name' => urlencode( get_bloginfo( 'name' ) ),
+							),
+							self::PUBLIC_URL . '/love-letter/'
+						);
+						?>
+						<a href="<?php echo esc_url( $love_letter_url ) ?>" target="_blank">
+							<?php echo esc_html_x( 'I have a secret', 'Haiku line 3', 'stream' ) ?>
 						</a>
 					</p>
 				</div>
