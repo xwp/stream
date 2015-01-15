9481444a99b24e032367b86209ffaa2ed55ef7d7
Frankie Jarrett <frankie@x-team.com>
2015-01-14 20:20:51 -0600

    Revert

--- classes/class-wp-stream-admin.php
+++ classes/class-wp-stream-admin.php
@@ -90,7 +90,6 @@ class WP_Stream_Admin {
 
 		// Admin notices
 		add_action( 'admin_notices', array( __CLASS__, 'admin_notices' ) );
-		add_action( 'admin_notices', array( __CLASS__, 'admin_connect_notice' ) );
 
 		// Show connect notice on dashboard and plugins pages
 		add_action( 'load-index.php', array( __CLASS__, 'prepare_connect_notice' ) );
@@ -136,12 +135,11 @@ class WP_Stream_Admin {
 	 * @return void
 	 */
 	public static function prepare_connect_notice() {
-		if ( ! current_user_can( self::SETTINGS_CAP ) || WP_Stream::is_connected() || WP_Stream::is_development_mode() ) {
-			return;
+		if ( ! WP_Stream::is_connected() && ! WP_Stream::is_development_mode() ) {
+			wp_enqueue_style( 'wp-stream-connect', WP_STREAM_URL . 'ui/css/connect.css', array(), WP_Stream::VERSION );
+			wp_enqueue_script( 'wp-stream-connect', WP_STREAM_URL . 'ui/js/connect.js', array(), WP_Stream::VERSION );
+			add_action( 'admin_notices', array( __CLASS__, 'admin_connect_notice' ) );
 		}
-
-		wp_enqueue_style( 'wp-stream-connect', WP_STREAM_URL . 'ui/css/connect.css', array(), WP_Stream::VERSION );
-		wp_enqueue_script( 'wp-stream-connect', WP_STREAM_URL . 'ui/js/connect.js', array(), WP_Stream::VERSION );
 	}
 
 	/**
@@ -150,7 +148,7 @@ class WP_Stream_Admin {
 	 * @return void
 	 */
 	public static function admin_connect_notice() {
-		if ( ! current_user_can( self::SETTINGS_CAP ) || WP_Stream::is_connected() || WP_Stream::is_development_mode() ) {
+		if ( ! current_user_can( self::SETTINGS_CAP ) ) {
 			return;
 		}
 
