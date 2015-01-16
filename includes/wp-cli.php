2eec6ca5f509462d09ec08e72c41b120e23c7b96
Frankie Jarrett <frankie@x-team.com>
2015-01-16 01:20:51 -0600

    Subcommands for help and account, query time added, check connections

--- includes/wp-cli.php
+++ includes/wp-cli.php
@@ -8,7 +8,7 @@
 class WP_Stream_WP_CLI_Command extends WP_CLI_Command {
 
 	/**
-	 * Query subcommand to retreive a list of activity records
+	 * Subcommand to query a set of Stream records
 	 *
 	 * You may use any associative argument(s) available in the
 	 * WP_Stream_Query::instance()->query() method.
@@ -21,6 +21,9 @@ class WP_Stream_WP_CLI_Command extends WP_CLI_Command {
 	 * @subcommand query
 	 */
 	public function query( $args, $assoc_args ) {
+		self::is_connected();
+
+		$start      = microtime( true );
 		$query_args = array();
 
 		foreach ( $assoc_args as $key => $value ) {
@@ -44,7 +47,6 @@ class WP_Stream_WP_CLI_Command extends WP_CLI_Command {
 
 		if ( empty( $records ) ) {
 			WP_CLI::success( __( 'No results found.', 'stream' ) );
-
 			return;
 		}
 
@@ -76,9 +78,83 @@ class WP_Stream_WP_CLI_Command extends WP_CLI_Command {
 		}
 
 		$found   = count( $records );
-		$message = sprintf( _n( '1 result found.', '%s results found.', $found, 'stream' ), number_format( $found ) );
+		$stop    = microtime( true ) - $start;
+		$message = sprintf( _n( '1 result found in %s seconds.', '%s results found in %s seconds.', $found, 'stream' ), number_format( $found ), number_format( $stop, 4 ) );
 
 		WP_CLI::success( $message );
+	}
+
+	/**
+	 * Subcommand to retreive details about your Stream account
+	 *
+	 * @subcommand account
+	 */
+	public function account() {
+		self::is_connected();
+
+		$site       = WP_Stream::$api->get_site();
+		$plan       = isset( $site->plan->type ) ? $site->plan->type : 'unknown';
+		$plan_label = ( 'free' === $plan ) ? __( 'Free', 'stream' ) : ( 'pro_monthly' === $plan ) ? __( 'Pro', 'stream' ) : ucwords( $plan );
+
+		WP_CLI::line( sprintf( __( 'Plan: %s', 'stream' ), $plan_label ) );
+
+		if ( 'free' !== $plan && 'pro_monthly' !== $plan ) {
+			return;
+		}
+
+		$retention   = isset( $site->plan->retention ) ? absint( $site->plan->retention ) : 0;
+		$retention   = ( 0 !== $retention ) ? sprintf( _n( '1 Day', '%s Days', $retention, 'stream' ), $retention ) : __( 'Unlimited', 'stream' );
+		$amount      = ! empty( $site->plan->amount ) ? $site->plan->amount : 0;
+		$expiry      = ! empty( $site->expiry->date ) ? $site->expiry->date : __( 'N/A', 'stream' );
+		$created     = isset( $site->created ) ? $site->created : null;
+		$date_format = get_option( 'date_format', 'F j, Y' );
+		$site_uuid   = get_option( 'wp_stream_site_uuid', null );
+		$api_key     = get_option( 'wp_stream_site_api_key', null );
+
+		WP_CLI::line( sprintf( __( 'Retention: %s', 'stream' ), $retention ) );
+
+		if ( 'free' !== $plan ) {
+			WP_CLI::line( sprintf( __( 'Next Billing: $%1$s on %2$s', 'stream' ), $amount, $expiry ) );
+		}
+
+		if ( $created ) {
+			WP_CLI::line( sprintf( __( 'Created: %s', 'stream' ), date_i18n( $date_format, strtotime( $created ) ) ) );
+		}
+
+		if ( $site_uuid ) {
+			WP_CLI::line( sprintf( __( 'Site ID: %s', 'stream' ), $site_uuid ) );
+		}
+
+		if ( $api_key ) {
+			WP_CLI::line( sprintf( __( 'API Key: %s', 'stream' ), $api_key ) );
+		}
+	}
+
+	/**
+	 * Subcommand for n00bs
+	 *
+	 * @subcommand help
+	 */
+	public function help() {
+		WP_CLI::line( __( 'Welcome to Stream for WP-CLI!', 'stream' ) );
+		WP_CLI::line( __( 'Available commands: query, account', 'stream' ) );
+	}
+
+	/**
+	 * Does a small query to see if records are retrievable
+	 *
+	 * @return bool
+	 */
+	private static function is_connected() {
+		WP_CLI::line( __( 'Establishing secure connection with Stream...', 'stream' ) );
+
+		$query = wp_stream_query( array( 'records_per_page' => 1, 'fields' => 'created' ) );
+
+		if ( ! $query ) {
+			WP_CLI::error( __( 'SITE IS DISCONNECTED', 'stream' ) );
+		} else {
+			WP_CLI::success( __( 'SITE IS CONNECTED', 'stream' ) );
+		}
 	}
 
 }
