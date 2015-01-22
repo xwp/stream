8f33be6a97ec329aa0d69f5e60512efc8cb6c695
Frankie Jarrett <frankie@x-team.com>
2015-01-22 16:58:13 -0600

    Removing account and help subcommands for simplicity

--- includes/wp-cli.php
+++ includes/wp-cli.php
@@ -85,68 +85,12 @@ class WP_Stream_WP_CLI_Command extends WP_CLI_Command {
 	}
 
 	/**
-	 * Subcommand to retreive details about your Stream account
-	 *
-	 * @subcommand account
-	 */
-	public function account() {
-		self::connection();
-
-		$site       = WP_Stream::$api->get_site();
-		$plan       = isset( $site->plan->type ) ? $site->plan->type : 'unknown';
-		$plan_label = ( 'free' === $plan ) ? __( 'Free', 'stream' ) : ( 'pro_monthly' === $plan ) ? __( 'Pro', 'stream' ) : ucwords( $plan );
-
-		WP_CLI::line( sprintf( __( 'Plan: %s', 'stream' ), $plan_label ) );
-
-		if ( 'free' !== $plan && 'pro_monthly' !== $plan ) {
-			return;
-		}
-
-		$retention   = isset( $site->plan->retention ) ? absint( $site->plan->retention ) : 0;
-		$retention   = ( 0 !== $retention ) ? sprintf( _n( '1 Day', '%s Days', $retention, 'stream' ), $retention ) : __( 'Unlimited', 'stream' );
-		$amount      = ! empty( $site->plan->amount ) ? $site->plan->amount : 0;
-		$expiry      = ! empty( $site->expiry->date ) ? $site->expiry->date : __( 'N/A', 'stream' );
-		$created     = isset( $site->created ) ? $site->created : null;
-		$date_format = get_option( 'date_format', 'F j, Y' );
-		$site_uuid   = get_option( 'wp_stream_site_uuid', null );
-		$api_key     = get_option( 'wp_stream_site_api_key', null );
-
-		WP_CLI::line( sprintf( __( 'Retention: %s', 'stream' ), $retention ) );
-
-		if ( 'free' !== $plan ) {
-			WP_CLI::line( sprintf( __( 'Next Billing: $%1$s on %2$s', 'stream' ), $amount, $expiry ) );
-		}
-
-		if ( $created ) {
-			WP_CLI::line( sprintf( __( 'Created: %s', 'stream' ), date_i18n( $date_format, strtotime( $created ) ) ) );
-		}
-
-		if ( $site_uuid ) {
-			WP_CLI::line( sprintf( __( 'Site ID: %s', 'stream' ), $site_uuid ) );
-		}
-
-		if ( $api_key ) {
-			WP_CLI::line( sprintf( __( 'API Key: %s', 'stream' ), $api_key ) );
-		}
-	}
-
-	/**
-	 * Subcommand for n00bs
-	 *
-	 * @subcommand help
-	 */
-	public function help() {
-		WP_CLI::line( __( 'Welcome to Stream for WP-CLI!', 'stream' ) );
-		WP_CLI::line( __( 'Available commands: query, account', 'stream' ) );
-	}
-
-	/**
 	 * Checks for a Stream connection and displays an error or success message
 	 *
 	 * @return void
 	 */
 	private static function connection() {
-		WP_CLI::line( __( 'Establishing secure connection with Stream...', 'stream' ) );
+		WP_CLI::line( __( 'Establishing secure connection with WP Stream...', 'stream' ) );
 
 		$query = wp_stream_query( array( 'records_per_page' => 1, 'fields' => 'created' ) );
 
