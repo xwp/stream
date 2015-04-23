7d38753d5918439ed969319e04e2d064a101b55d
Frankie Jarrett <frankie@x-team.com>
2015-04-23 11:00:02 -0500

    Simplify conditional logic

--- connectors/class-wp-stream-connector-taxonomies.php
+++ connectors/class-wp-stream-connector-taxonomies.php
@@ -94,7 +94,7 @@ class WP_Stream_Connector_Taxonomies extends WP_Stream_Connector {
 					$term_id = wp_get_split_term( $term->term_id, $term->taxonomy );
 				}
 
-				$term_id = ! empty( $term_id ) ? $term_id : $term->term_id;
+				$term_id = empty( $term_id ) ? $term->term_id : $term_id;
 
 				$links[ sprintf( _x( 'Edit %s', 'Term singular name', 'stream' ), $tax_label ) ] = get_edit_term_link( $term_id, $term->taxonomy );
 				$links[ __( 'View', 'stream' ) ] = get_term_link( $term_id, $term->taxonomy );
