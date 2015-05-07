4ea3169105c8092f35471416c8be5f7f3efae38c
Frankie Jarrett <frankie@x-team.com>
2015-05-07 17:43:42 -0500

    Use wp_json_encode instead of json_encode in feeds

--- includes/feeds/json.php
+++ includes/feeds/json.php
@@ -1,7 +1,7 @@
 <?php
 header( 'Content-type: application/json; charset=' . get_option( 'blog_charset' ), true );
 if ( version_compare( PHP_VERSION, '5.4', '>=' ) ) {
-	echo json_encode( $records, JSON_PRETTY_PRINT );
+	echo wp_json_encode( $records, JSON_PRETTY_PRINT );
 } else {
-	echo json_encode( $records );
+	echo wp_json_encode( $records );
 }
