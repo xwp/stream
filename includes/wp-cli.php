70448c653f0eab4d78e44ca637f10e2e5662777a
Frankie Jarrett <frankie@x-team.com>
2015-05-08 13:29:31 -0500

    Proper support for JSON_PRETTY_PRINT when less than 5.4

--- includes/wp-cli.php
+++ includes/wp-cli.php
@@ -148,7 +148,11 @@ class WP_Stream_WP_CLI_Command extends WP_CLI_Command {
 			}
 
 			if ( 'json_pretty' === $assoc_args['format'] ) {
-				WP_CLI::line( wp_stream_json_encode( $formatted_records, JSON_PRETTY_PRINT ) );
+				if ( version_compare( PHP_VERSION, '5.4', '<' ) ) {
+					WP_CLI::line( wp_stream_json_encode( $formatted_records ) ); // xss ok
+				} else {
+					WP_CLI::line( wp_stream_json_encode( $formatted_records, JSON_PRETTY_PRINT ) ); // xss ok
+				}
 			}
 
 			if ( 'csv' === $assoc_args['format'] ) {
