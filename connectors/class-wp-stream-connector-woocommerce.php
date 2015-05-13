88cc7362a94d2acea119928a558063ce9193b6a8
Frankie Jarrett <frankie@x-team.com>
2015-05-12 21:19:47 -0500

    Less code for vip conditionals

--- connectors/class-wp-stream-connector-woocommerce.php
+++ connectors/class-wp-stream-connector-woocommerce.php
@@ -399,13 +399,8 @@ class WP_Stream_Connector_Woocommerce extends WP_Stream_Connector {
 			return;
 		}
 
-		if ( WP_Stream::is_vip() ) {
-			$old_status = wpcom_vip_get_term_by( 'slug', $old, 'shop_order_status' );
-			$new_status = wpcom_vip_get_term_by( 'slug', $new, 'shop_order_status' );
-		} else {
-			$old_status = get_term_by( 'slug', $old, 'shop_order_status' );
-			$new_status = get_term_by( 'slug', $new, 'shop_order_status' );
-		}
+		$old_status = WP_Stream::is_vip() ? wpcom_vip_get_term_by( 'slug', $old, 'shop_order_status' ) : get_term_by( 'slug', $old, 'shop_order_status' );
+		$new_status = WP_Stream::is_vip() ?wpcom_vip_get_term_by( 'slug', $new, 'shop_order_status' ) : get_term_by( 'slug', $new, 'shop_order_status' );
 
 		// Don't track new statuses
 		if ( ! $old_status ) {
