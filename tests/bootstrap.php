5f3946431658d70ff4538083cdde705b22047946
Frankie Jarrett <frankie@x-team.com>
2015-06-25 19:56:13 -0500

    Fix missing slash in bootstrap require line

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -14,7 +14,7 @@ $_tests_dir = getenv('WP_TESTS_DIR');
 if ( ! $_tests_dir ) {
 	$_tests_dir = '/tmp/wordpress-tests-lib/';
 }
-require_once $_tests_dir . 'includes/functions.php';
+require_once $_tests_dir . '/includes/functions.php';
 
 tests_add_filter(
 	'muplugins_loaded',
