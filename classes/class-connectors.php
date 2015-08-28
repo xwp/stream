2e44b6b84efc7e945d185a69075a0e69bcccc453
John Blackbourn <johnbillion@gmail.com>
2015-08-28 10:58:30 +0200

    Correct the docs for the `wp_stream_connectors` filter.

--- classes/class-connectors.php
+++ classes/class-connectors.php
@@ -103,7 +103,7 @@ class Connectors {
 		/**
 		 * Allows for adding additional connectors via classes that extend Connector.
 		 *
-		 * @param array $classes An array of connector class names.
+		 * @param array $classes An array of Connector objects.
 		 */
 		$this->connectors = apply_filters( 'wp_stream_connectors', $classes );
 
