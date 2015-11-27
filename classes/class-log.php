b6a9e694aab4ba950df75fc11eca94e19bf89899
Luke Carbis <luke@xwp.co>
2015-11-27 11:33:29 +1000

    Check if function exists before calling is_plugin_active_for_network

--- classes/class-log.php
+++ classes/class-log.php
@@ -160,7 +160,12 @@ class Log {
 
 		$exclude_settings = isset( $this->plugin->settings->options['exclude_rules'] ) ? $this->plugin->settings->options['exclude_rules'] : array();
 
-		if ( is_multisite() && is_plugin_active_for_network( $this->plugin->locations['plugin'] ) && ! is_network_admin() ) {
+		if (
+			is_multisite() &&
+			function_exists( 'is_plugin_active_for_network' ) &&
+			is_plugin_active_for_network( $this->plugin->locations['plugin'] ) &&
+			! is_network_admin()
+		) {
 			$multisite_options = (array) get_site_option( 'wp_stream_network', array() );
 			$multisite_exclude_settings = isset( $multisite_options['exclude_rules'] ) ? $multisite_options['exclude_rules'] : array();
 
