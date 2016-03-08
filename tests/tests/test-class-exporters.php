6514a5c36f500c823673233a1e773b084fcf89a0
Tyler Carter <tyler@tylercarter.org>
2016-03-07 17:30:19 -0800

    Add CSV Exporter Test

--- tests/tests/test-class-exporters.php
+++ tests/tests/test-class-exporters.php
@@ -11,4 +11,12 @@ class Test_Exporters extends WP_StreamTestCase {
 		$exporter->output_file( $array );
 	}
 
+	public function test_csv_export() {
+		$exporter = new Exporter_CSV;
+
+		$array = array( 'key' => 'value' );
+		$this->expectOutputString( join( ',', $array ) );
+		$exporter->output_file( $array );
+	}
+
 }
