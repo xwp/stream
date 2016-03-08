cdc4942d337e9a4908d54c3f39a7c372daaaaab6
Tyler Carter <tyler@tylercarter.org>
2016-03-08 11:15:44 -0800

    Add test coverage for Export class

--- tests/tests/test-class-export.php
+++ tests/tests/test-class-export.php
@@ -9,30 +9,149 @@ class Test_Export extends WP_StreamTestCase {
 	 */
 	protected $export;
 
+	/**
+	 * Set up for tests
+	 */
 	public function setUp() {
 		parent::setUp();
-
 		$this->export = $this->plugin->admin->export;
 		$this->assertNotEmpty( $this->export );
+		$this->assertEmpty( $this->export->exporters );
 	}
 
-	public function test_init() {
+	/**
+	 * Test class constructor
+	 */
+	public function test_construct() {
+		$this->assertNotEmpty( $this->export->plugin );
+		$this->assertNotEmpty( $this->export->admin );
+
 		$exporters = $this->export->exporters;
 		$this->assertEmpty( $exporters );
 	}
 
 	/**
+	 * Test that render download uses selected renderer
+	 */
+	public function test_render_download() {
+
+		$_GET['output'] = 'csv';
+		$this->export->register_exporter( new Exporter_CSV );
+
+		ob_start();
+		$this->export->render_download();
+		$output = ob_get_clean();
+
+		$this->assertNotEmpty( $output );
+		$this->assertStringStartsWith( 'Date,Summary,User,Connector,Context,Action,IP Address', $output );
+
+		$this->export->exporters = array(); // Clean up.
+		unset( $_GET['output'] );
+	}
+
+	/**
+	 * Test no output on normal page load
+	 */
+	public function test_render_output_blank() {
+		ob_start();
+		$this->export->render_download();
+		$output = ob_get_clean();
+
+		$this->assertEmpty( $output );
+	}
+
+	/**
+	 * Test that record building grab correct columns
+	 */
+	public function test_build_record() {
+
+		$columns = array( 'connector' => '' );
+		$data    = $this->dummy_stream_data;
+		$output  = $this->export->build_record( $data, $columns );
+
+		$this->assertNotEmpty( $output );
+		$this->assertArrayHasKey( 'connector', $output );
+		$this->assertEquals( $data['connector'], $output['connector'] );
+
+		$columns = array( 'context' => '' );
+		$output  = $this->export->build_record( $data, $columns );
+
+		$this->assertNotEmpty( $output );
+		$this->assertArrayNotHasKey( 'connector', $output );
+		$this->assertArrayHasKey( 'context', $output );
+		$this->assertEquals( $data['context'], $output['context'] );
+
+	}
+
+	/**
+	 * Test pagination limit is increased
+	 */
+	public function test_disable_paginate() {
+		$limit = $this->export->disable_paginate( 0 );
+		$this->assertEquals( $limit, 10000 );
+	}
+
+	/**
+	 * Test for present columns returning
+	 */
+	public function test_expand_columns() {
+		$test_data = array(
+		 'date'		=> '',
+		 'summary' => '',
+		 'user_id' => '',
+		 'context' => '',
+		 'action'	=> '',
+		 'ip'			=> '',
+		);
+		$columns = $this->export->expand_columns( $test_data );
+
+		$this->assertArrayHasKey( 'date', $columns );
+		$this->assertArrayHasKey( 'summary', $columns );
+		$this->assertArrayHasKey( 'user_id', $columns );
+		$this->assertArrayHasKey( 'connector', $columns );
+		$this->assertArrayHasKey( 'context', $columns );
+		$this->assertArrayHasKey( 'action', $columns );
+		$this->assertArrayHasKey( 'ip', $columns );
+	}
+
+	/**
+	 * Test register a valid class adds it to the list.
+	 */
+	public function test_register_exporter() {
+		$this->assertEmpty( $this->export->exporters );
+		$this->export->register_exporter( new Exporter_CSV );
+		$this->assertNotEmpty( $this->export->exporters );
+
+		$this->export->exporters = array(); // Clean up.
+	}
+
+	/**
 	 * Test registering a invalid class type produces an error
+	 *
 	 * @expectedException PHPUnit_Framework_Error
 	 */
-	public function test_register_invalid_class() {
+	public function test_register_exporter_invalid_class() {
 		$this->export->register_exporter( new \stdClass );
+	}
+
+	/**
+	 * Valid classes added are returned.
+	 */
+	public function test_get_exporters() {
+		$this->assertEmpty( $this->export->get_exporters() );
+		$this->export->register_exporter( new Exporter_CSV );
+
+		$exporters = $this->export->get_exporters();
+		$this->assertNotEmpty( $exporters );
+		$this->assertArrayHasKey( 'csv', $exporters );
+
+		$this->export->exporters = array(); // Clean up.
 	}
 
 	/**
 	 * Test default classes are only registered on WP_Stream page
 	 */
-	public function test_register_default_class() {
+	public function test_register_default_exporters() {
 
 		// Test registration on stream page.
 		$_GET['page'] = 'wp_stream';
@@ -51,33 +170,22 @@ class Test_Export extends WP_StreamTestCase {
 	}
 
 	/**
-	 * Test for present columns returning
+	 * Return dummy stream data
 	 */
-	 public function test_expand_columns() {
-		 $test_data = array(
-			 'date'    => '',
-			 'summary' => '',
-			 'user_id' => '',
-			 'context' => '',
-			 'action'  => '',
-			 'ip'      => '',
-		 );
-		 $columns = $this->export->render_csv_expand_columns( $test_data );
-
-		 $this->assertArrayHasKey( 'date', $columns );
-		 $this->assertArrayHasKey( 'summary', $columns );
-		 $this->assertArrayHasKey( 'user_id', $columns );
-		 $this->assertArrayHasKey( 'connector', $columns );
-		 $this->assertArrayHasKey( 'context', $columns );
-		 $this->assertArrayHasKey( 'action', $columns );
-		 $this->assertArrayHasKey( 'ip', $columns );
-	 }
-
-	 /**
-	  * Test no output on normal page load
-		*/
-		public function test_render_output_blank() {
-			$this->export->render_download();
-		}
+	private function dummy_stream_data() {
+		return array(
+			'object_id' => null,
+			'site_id' => '1',
+			'blog_id' => get_current_blog_id(),
+			'user_id' => '1',
+			'user_role' => 'administrator',
+			'created' => date( 'Y-m-d H:i:s' ),
+			'summary' => '"Hello Dave" plugin activated',
+			'ip' => '192.168.0.1',
+			'connector' => 'installer',
+			'context' => 'plugins',
+			'action' => 'activated',
+		);
+	}
 
 }
