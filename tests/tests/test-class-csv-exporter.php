2150fa6ee5ba0ba74e198bde8c146eaf997f44d3
Tyler Carter <tyler@tylercarter.org>
2016-03-07 17:47:40 -0800

    Separate exporter test classes

---
+++ tests/tests/test-class-csv-exporter.php
@@ -0,0 +1,12 @@
+<?php
+class Test_CSV_Exporters extends WP_StreamTestCase {
+
+	public function test_csv_export() {
+		$exporter = new Exporter_CSV;
+
+		$array = array( 'key' => 'value' );
+		$this->expectOutputString( join( ',', $array ) );
+		$exporter->output_file( $array );
+	}
+
+}
---
+++ tests/tests/test-class-json-exporter.php
@@ -0,0 +1,14 @@
+<?php
+namespace WP_Stream;
+
+class Test_JSON_Exporters extends WP_StreamTestCase {
+
+	public function test_json_export() {
+		$exporter = new Exporter_JSON;
+
+		$array = array( 'key' => 'value' );
+		$this->expectOutputString( json_encode( $array ) );
+		$exporter->output_file( $array );
+	}
+
+}
