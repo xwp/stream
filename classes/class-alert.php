b5ff3aebe05282742e5371f59cd5324e59488c41
Tyler Carter <tyler@tylercarter.org>
2016-03-17 21:15:51 -0700

    Create alert data class

---
+++ classes/class-alert.php
@@ -0,0 +1,114 @@
+<?php
+namespace WP_Stream;
+
+class Alert {
+	public $ID;
+	public $date;
+	public $author;
+
+	public $filter_author;
+	public $filter_action;
+	public $filter_context;
+	public $alert_type;
+	public $alert_meta;
+
+	public function __construct( $item ) {
+
+		$this->ID      = isset( $item->ID ) ? $item->ID : null;
+		$this->date    = isset( $item->date ) ? $item->date : null;
+		$this->author = isset( $item->author ) ? $item->author : null;
+
+		$this->filter_action  = isset( $item->filter_action ) ? $item->filter_action : null;
+		$this->filter_author  = isset( $item->filter_author ) ? $item->filter_author : null;
+		$this->filter_context = isset( $item->filter_context ) ? $item->filter_context : null;
+
+		$this->alert_type = isset( $item->alert_type ) ? $item->alert_type : null;
+		$this->alert_meta = isset( $item->alert_meta ) ? $item->alert_meta : null;
+	}
+
+	public function save() {
+		if ( ! $this->validate() ) {
+			return new \WP_Error( 'validation-error', esc_html__( 'Could not validate record data.', 'stream' ) );
+		}
+
+		$args = array(
+			'ID'           => $this->ID,
+			'post_date'    => $this->date,
+			'post_content' => '',
+			'post_title'   => 'Test Title',
+			'post_author'  => $this->author,
+		);
+		$post_id = wp_insert_post( $args );
+		if ( 0 === $post_id ) {
+			return false;
+		} else if ( null == $this->ID ) {
+			$this->ID = $post_id;
+		}
+
+		$meta = array(
+			'filter_action'  => $this->filter_action,
+			'filter_author'  => $this->filter_author,
+			'filter_context' => $this->filter_context,
+			'alert_type'     => $this->alert_type,
+			'alert_meta'     => $this->alert_meta,
+		);
+		foreach( $meta as $key => $value ) {
+			$this->update_meta( $key, $value );
+		}
+
+		return true;
+	}
+
+	public function populate( array $raw ) {
+		$keys = get_class_vars( $this );
+		$data = array_intersect_key( $raw, $keys );
+		foreach ( $data as $key => $val ) {
+			$this->{$key} = $val;
+		}
+	}
+
+	public function validate() {
+		return true;
+	}
+
+	/**
+	 * Query record meta
+	 *
+	 * @param string $meta_key (optional)
+	 * @param bool   $single (optional)
+	 *
+	 * @return array
+	 */
+	public function get_meta( $meta_key = '', $single = false ) {
+		return maybe_unserialize( get_post_meta( $this->ID, $meta_key, $single ) );
+	}
+
+	/**
+	 * Update record meta
+	 *
+	 * @param string $meta_key
+	 * @param string $meta_value
+	 * @param string $prev_value (optional)
+	 *
+	 * @return array
+	 */
+	public function update_meta( $meta_key, $meta_value, $prev_value = '' ) {
+		return update_post_meta( $this->ID, $meta_key, $meta_value, $prev_value );
+	}
+
+	/**
+	 * Determine the title of an object that a record is for.
+	 *
+	 * @param  object  Record object
+	 * @return mixed   The title of the object as a string, otherwise false
+	 */
+	function get_object_title() {
+		if ( ! isset( $this->object_id ) || empty( $this->object_id ) ) {
+			return false;
+		}
+
+		trigger_error( 'Alert::get_object_title() is not implemented yet.' );
+
+		return $output;
+	}
+}
---
+++ tests/tests/test-class-alert.php
@@ -0,0 +1,70 @@
+<?php
+namespace WP_Stream;
+
+class Test_Alert extends WP_StreamTestCase {
+
+  function test_construct() {
+    $data  = $this->get_dummy_data();
+    $alert = new Alert( $data );
+
+    $this->assertNotEmpty( $alert->ID );
+    $this->assertNotEmpty( $alert->date );
+    $this->assertNotEmpty( $alert->author );
+    $this->assertNotEmpty( $alert->filter_action );
+    $this->assertNotEmpty( $alert->filter_author );
+    $this->assertNotEmpty( $alert->filter_context );
+    $this->assertNotEmpty( $alert->alert_type );
+    $this->assertNotEmpty( $alert->alert_meta );
+  }
+
+  function test_save() {
+    $data     = $this->get_dummy_data();
+    $data->ID = null;
+    $alert    = new Alert( $data );
+
+    $post = get_post( $alert->ID );
+    $this->assertEquals( $post, 0 );
+
+    $status = $alert->save();
+    $this->assertTrue( $status );
+
+    $post = get_post( $alert->ID );
+    $this->assertEquals( $alert->ID, $post->ID );
+    $this->assertEquals( $alert->date, $post->post_date );
+    $this->assertEquals( $alert->author, $post->post_author );
+
+    $fields = array(
+      'filter_action',
+      'filter_author',
+      'filter_context',
+      'alert_type',
+      'alert_meta'
+    );
+    foreach( $fields as $field ) {
+      $actual = get_post_meta( $alert->ID, $field, true );
+      $this->assertEquals( $alert->$field, $actual );
+    }
+
+    $alert->date = date( 'Y-m-d H:i:s', 0 );
+    $alert->save();
+
+    $post = get_post( $alert->ID );
+    $this->assertEquals( $post->post_date, $alert->date );
+  }
+
+  function get_dummy_data() {
+    return (object) array(
+      'ID' => 1,
+      'date' => date( 'Y-m-d H:i:s' ),
+      'author' => '1',
+      'filter_action'  => 'activated',
+      'filter_author'  => 'administrator',
+      'filter_context' => 'plugins',
+      'alert_type'     => 'highlight',
+      'alert_meta'     => array(
+        'key1'         => 'value1',
+        'key2'         => 'value2',
+      ),
+    );
+  }
+}
