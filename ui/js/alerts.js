64892eb8909c1e31f62d4a4dadf6ed7e943d3cab
Tyler Carter <tyler@tylercarter.org>
2016-04-04 18:13:51 -0700

    Activate select boxes

--- ui/js/alerts.js
+++ ui/js/alerts.js
@@ -1,2 +1,10 @@
 jQuery( function( $ ) {
+  $( '.chosen-select' ).each( function( i ) {
+    var $input = $( this );
+    $input.select2( {
+      data: $input.data( 'values' ),
+      allowClear: true,
+      placeholder: $input.data( 'placeholder' ),
+    } );
+  } );
 } );
