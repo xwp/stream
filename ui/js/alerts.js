53bc526e09a4d98d30916c6c8b11d40abcfabe05
Tyler Carter <tyler@tylercarter.org>
2016-04-04 18:54:28 -0700

    Remove unused variable

--- ui/js/alerts.js
+++ ui/js/alerts.js
@@ -1,10 +1,10 @@
 jQuery( function( $ ) {
-  $( '.chosen-select' ).each( function( i ) {
+  $( '.chosen-select' ).each( function() {
     var $input = $( this );
     $input.select2( {
       data: $input.data( 'values' ),
       allowClear: true,
-      placeholder: $input.data( 'placeholder' ),
+      placeholder: $input.data( 'placeholder' )
     } );
   } );
 } );
