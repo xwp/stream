0e2b374c2754bf93b25d2a46efdd94ee1dae466b
Tyler Carter <tyler@tylercarter.org>
2016-04-08 18:55:30 -0700

    Fix spaces vs tabs

--- classes/class-notifier.php
+++ classes/class-notifier.php
@@ -3,16 +3,16 @@ namespace WP_Stream;
 
 abstract class Notifier {
 
-  /**
-   *  Notify receipients about the new record
-   *
-   * @param array $recordarr Record details
-   * @param array $options Notification options
-   *
-   */
-  abstract public function notify( $recordarr, $options );
+	/**
+	 *	Notify receipients about the new record
+	 *
+	 * @param array $recordarr Record details
+	 * @param array $options Notification options
+	 *
+	 */
+	abstract public function notify( $recordarr, $options );
 
-  /**
+	/**
 	 * Allow connectors to determine if their dependencies is satisfied or not
 	 *
 	 * @return bool
@@ -20,5 +20,4 @@ abstract class Notifier {
 	public function is_dependency_satisfied() {
 		return true;
 	}
-
 }
