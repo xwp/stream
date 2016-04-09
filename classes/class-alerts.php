2bf07f82ceb83ab2dc55dc23320d244c63660ccb
Tyler Carter <tyler@tylercarter.org>
2016-04-08 18:59:13 -0700

    Remove Notifier_Die from production

--- classes/class-alerts.php
+++ classes/class-alerts.php
@@ -43,7 +43,6 @@ class Alerts {
 
 	function load_notifiers() {
 		$notifiers = array(
-			'die',
 			'null',
 			'menu-alert',
 		);
--- notifiers/class-notifier-die.php
+++ notifiers/class-notifier-die.php
@@ -3,25 +3,24 @@ namespace WP_Stream;
 
 class Notifier_Die extends Notifier {
 
-  /**
+	/**
 	 * Notifier name
 	 *
 	 * @var string
 	 */
-  public $name = 'Die Notifier';
+	public $name = 'Die Notifier';
 
-  /**
+	/**
 	 * Notifier slug
 	 *
 	 * @var string
 	 */
-  public $slug = 'die';
+	public $slug = 'die';
 
-  public function notify( $recordarr, $options ) {
-    echo '<pre>';
-    print_r( $recordarr );
-    echo '</pre>';
-    die( "You have been notified!" );
-  }
-
+	public function notify( $recordarr, $options ) {
+		echo '<pre>';
+		print_r( $recordarr ); // @codingStandardsIgnoreLine debug not loaded in production
+		echo '</pre>';
+		die( 'You have been notified!' );
+	}
 }
