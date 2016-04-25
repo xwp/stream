adda65ac784e86b3494bfccc5f47c874e656529a
Tyler Carter <tyler@tylercarter.org>
2016-04-25 12:06:19 -0700

    Add form generator inputs

--- classes/class-form-generator.php
+++ classes/class-form-generator.php
@@ -32,6 +32,24 @@ class Form_Generator {
 
 		$output = '';
 		switch ( $field_type ) {
+			case 'select':
+				$current_value = $args['value'];
+
+				$output  = sprintf(
+					'<select name="%1$s">',
+					esc_attr( $args['name'] )
+				);
+
+				foreach ( $args['options'] as $value => $label ) {
+					$output .= sprintf(
+						'<option value="%1$s" %2$s>%3$s</option>',
+						esc_attr( $value ),
+						checked( $value === $current_value, true, false ),
+						esc_html( $label )
+					);
+				}
+				$output .= '</select>';
+				break;
 			case 'select2':
 				$args = wp_parse_args( $original_args, array(
 					'name'        => '',
@@ -45,6 +63,14 @@ class Form_Generator {
 					esc_attr( $args['value'] ),
 					esc_attr( wp_stream_json_encode( $args['options'] ) ),
 					esc_attr( $args['placeholder'] )
+				);
+				break;
+			case 'checkbox':
+				$output = sprintf(
+					'<input type="checkbox" name="%1$s" value="1" %3$s>%2$s',
+					$args['name'],
+					$args['text'],
+					checked( $args['value'], true, false )
 				);
 				break;
 			default:
