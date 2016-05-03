a57b1cb11c6968b51c95eb9825d33b4d73f4b7e3
Tyler Carter <tyler@tylercarter.org>
2016-05-02 17:45:12 -0700

    Allow clear in filters

--- ui/js/alerts.js
+++ ui/js/alerts.js
@@ -3,6 +3,7 @@ jQuery( function( $ ) {
     var $input = $( this );
     $input.select2( {
       data: $input.data( 'values' ),
+      allowClear: true,
       placeholder: $input.data( 'placeholder' )
     } );
   } );
