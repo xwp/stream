60468711ecaca77504537e25e7c6b96480755278
Luke Carbis <luke@xwp.co>
2016-06-14 13:11:28 +1000

    Restore a deleted comment

--- classes/class-uninstall.php
+++ classes/class-uninstall.php
@@ -135,6 +135,7 @@ class Uninstall {
 		// Wildcard matches on network options
 		$wpdb->query( "DELETE FROM {$wpdb->sitemeta} WHERE meta_key LIKE '%wp_stream%';" );
 
+		// Delete options from each blog on network
 		foreach ( wp_stream_get_sites() as $blog ) {
 			$this->delete_blog_options( absint( $blog['blog_id'] ) );
 		}
