841e78337d87827ca3e27f134d5bbe6538ea7efb
Luke Carbis <luke@xwp.co>
2016-10-31 15:19:42 +1000

    Add link to a video tutorial for IFTTT

--- alerts/class-alert-type-ifttt.php
+++ alerts/class-alert-type-ifttt.php
@@ -111,7 +111,10 @@ class Alert_Type_IFTTT extends Alert_Type {
 
 		$form = new Form_Generator;
 
-		echo '<span class="wp_stream_alert_type_description">' . esc_html__( 'Trigger an IFTTT Maker recipe.', 'stream' ) . '</span>';
+		echo '<span class="wp_stream_alert_type_description">';
+		echo esc_html__( 'Trigger an IFTTT Maker recipe.', 'stream' );
+		echo sprintf( ' (<a href="%1$s" target="_blank">%2$s</span></a>)', 'https://youtu.be/XFWtkHXv9h0', __( 'Tutorial', 'stream' ) );
+		echo '</span>';
 		echo '<label for="wp_stream_ifttt_maker_key"><span class="title">' . esc_html__( 'Maker Key', 'stream' ) . '</span>';
 		echo '<span class="input-text-wrap">';
 		echo $form->render_field( 'text', array( // Xss ok.
