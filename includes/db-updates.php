2c5d7e25ef35bc6a02df0fe5af27be2d0347271a
John Oleksowicz <john.oleksowicz@govdelivery.com>
2017-01-10 11:12:45 -0600

    Move another multiline function call to single line

--- includes/db-updates.php
+++ includes/db-updates.php
@@ -68,8 +68,7 @@ function wp_stream_update_auto_300( $db_version, $current_version ) {
 	while ( ! empty( $stream_entries ) ) {
 		foreach ( $stream_entries as $entry ) {
 			$context = $wpdb->get_row(
-				$wpdb->prepare( "SELECT * FROM {$wpdb->base_prefix}stream_context_tmp WHERE record_id = %s LIMIT 1",
-					$entry->ID )
+				$wpdb->prepare( "SELECT * FROM {$wpdb->base_prefix}stream_context_tmp WHERE record_id = %s LIMIT 1", $entry->ID )
 			);
 
 			$new_entry = array(
