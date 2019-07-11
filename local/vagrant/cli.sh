9ffdcb9404dddc01e84862e8a98ec213ae6abb08
Kaspars Dambis <hi@kaspars.net>
2019-07-11 17:49:20 +0300

    Don’t assume a container is up

--- local/vagrant/cli.sh
+++ local/vagrant/cli.sh
@@ -7,4 +7,4 @@ DOCKER_COMPOSE_FILE=${DOCKER_COMPOSE_FILE:-"docker-compose.yml"}
 
 # Run the command passed to this script (captured by $@) inside the WP environment.
 # Set --user to 1000 to allow file writes and -T to disable the TTL thing.
-docker-compose --file $DOCKER_COMPOSE_FILE exec --user 1000 -T wordpress "${@}"
+docker-compose --file $DOCKER_COMPOSE_FILE run --user 1000 -T wordpress "${@}"
