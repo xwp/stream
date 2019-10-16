2c876474a651e3ea2b55768daba6debe214ab9d1
Prasath Nadarajah <n.prasath.002@gmail.com>
2019-10-16 15:22:43 +0530

    Use unique kets for stream meta

--- classes/class-db-driver-wpdb.php
+++ classes/class-db-driver-wpdb.php
@@ -68,8 +68,8 @@ class DB_Driver_WPDB implements DB_Driver {
 
 		// Insert record meta
 		foreach ( (array) $meta as $key => $vals ) {
-			foreach ( (array) $vals as $val ) {
-				$this->insert_meta( $record_id, $key, $val );
+			foreach ( (array) $vals as $meta_key $val ) {
+				$this->insert_meta( $record_id, $meta_key, $val );
 			}
 		}
 
