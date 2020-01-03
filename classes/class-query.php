760f0601b433adebced9ee5d93a61dad32ee2b65
Prasath Nadarajah <n.prasath.002@gmail.com>
2020-01-03 12:01:27 +0530

    Fix stream db query

--- classes/class-query.php
+++ classes/class-query.php
@@ -116,8 +116,8 @@ class Query {
 					continue;
 				}
 
-				$field = str_replace( array( 'record_', '__in' ), '', $key );
-				$field = empty( $field ) ? 'ID' : $field;
+				$field = str_replace( '__in', '', $key );
+				$field = ( $field === 'record' ) ? 'ID' : $field;
 				$type  = is_numeric( array_shift( $value ) ) ? '%d' : '%s';
 
 				if ( ! empty( $value ) ) {
@@ -144,8 +144,8 @@ class Query {
 					continue;
 				}
 
-				$field = str_replace( array( 'record_', '__not_in' ), '', $key );
-				$field = empty( $field ) ? 'ID' : $field;
+				$field = str_replace( '__not_in', '', $key );
+				$field = ( $field === 'record' ) ? 'ID' : $field;
 				$type  = is_numeric( array_shift( $value ) ) ? '%d' : '%s';
 
 				if ( ! empty( $value ) ) {
