891f20bfaa3e2e3357bd03ecaaf7744a0f5126e0
Kaspars Dambis <hi@kaspars.net>
2020-03-20 12:50:11 +0200

    Debug the clone

--- local/scripts/dist.sh
+++ local/scripts/dist.sh
@@ -19,7 +19,7 @@ export GIT_WORK_TREE="$DIST_DIR"
 # Start with a clean slate.
 rm -rf "$DIST_DIR"
 
-git clone "$DIST_REPO" "$DIST_DIR/.git"
+git clone --progress --verbose "$DIST_REPO" "$DIST_DIR/.git"
 git checkout -B $DIST_BRANCH
 
 # Copy over the update.
