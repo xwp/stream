1cddb6021083b63cb937b6413fe6869a164f5fc2
Kaspars Dambis <hi@kaspars.net>
2020-07-07 13:30:28 +0300

    Lint dev files too
    But don’t require a few ruls

--- local/scripts/make-clover-relative.php
+++ local/scripts/make-clover-relative.php
@@ -7,9 +7,9 @@
 $clover_xml_file = $argv[1]; // First argument must be path to the XML coverage file.
 $clover_root_dir = sprintf( '%s/', getcwd() );
 
-$xml = file_get_contents( $clover_xml_file );
+$xml = file_get_contents( $clover_xml_file ); // phpcs:ignore WordPress.WP.AlternativeFunctions.file_get_contents_file_get_contents, used only during dev.
 
-file_put_contents(
+file_put_contents( // phpcs:ignore WordPress.WP.AlternativeFunctions.file_system_read_file_put_contents, used only during dev.
 	$clover_xml_file,
 	str_replace( $clover_root_dir, '', $xml ) // Make coverage path relative to the project root.
 );
