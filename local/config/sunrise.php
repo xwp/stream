c6824ab7426724e1a0ad9738b743d4e2ee3f75ab
Geoff Taylor <geoffrey.taylor@outlook.com>
2020-09-25 17:33:38 -0400

    Mercator connector test implemented.

---
+++ local/config/sunrise.php
@@ -0,0 +1,5 @@
+<?php
+// Default mu-plugins directory if you haven't set it
+defined( 'WPMU_PLUGIN_DIR' ) or define( 'WPMU_PLUGIN_DIR', WP_CONTENT_DIR . '/mu-plugins' );
+
+require WPMU_PLUGIN_DIR . '/mercator/mercator.php';
