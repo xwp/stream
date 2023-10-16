560baf1f86dc15d95041a68b087d07a817626fc6
Kaspars Dambis <kaspars.dambis@xwp.co>
2023-10-16 09:59:33 +0300

    Skip any formatting for simplicity

--- classes/class-admin.php
+++ classes/class-admin.php
@@ -224,7 +224,7 @@ class Admin {
 		// Check if the host has configured the `REMOTE_ADDR` correctly.
 		$client_ip = $this->plugin->get_client_ip_address();
 		if ( ! empty( $client_ip ) && $this->is_stream_screen() ) {
-			$this->notice( __( 'Stream plugin can\'t determine a reliable client IP address! Please update the hosting environment to set the <code>$_SERVER[\'REMOTE_ADDR\']</code> variable or use the <code>wp_stream_client_ip_address</code> filter to specify the verified client IP address!', 'stream' ) );
+			$this->notice( __( 'Stream plugin can\'t determine a reliable client IP address! Please update the hosting environment to set the $_SERVER[\'REMOTE_ADDR\'] variable or use the wp_stream_client_ip_address filter to specify the verified client IP address!', 'stream' ) );
 		}
 	}
 
