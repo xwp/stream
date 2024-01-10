94da5910b23c0cfce1dddb16e3d7124d467a11e1
Justin Maurer <justin.maurer@reason.com>
2024-01-10 11:16:51 -0600

    WPCS style compliance

--- classes/class-log.php
+++ classes/class-log.php
@@ -194,8 +194,8 @@ class Log {
 				'role'       => ( ! empty( $exclude_rule['author_or_role'] ) && ! is_numeric( $exclude_rule['author_or_role'] ) ) ? $exclude_rule['author_or_role'] : null,
 			);
 
-			$exclude_rules = array_filter($exclude, function ($val) {
-				return !is_null($val) && strlen($val);
+			$exclude_rules = array_filter( $exclude, function ( $val ) {
+				return !is_null( $val ) && strlen( $val );
 			});
 
 			if ( $this->record_matches_rules( $record, $exclude_rules ) ) {
