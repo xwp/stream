a613500827c825a0897f7f4e1e9cfa6af80be6cb
Marcin Krzemiński <marcin@krzeminski.net>
2024-12-31 12:32:33 +0100

    Revert "Revert tags-suggest deregistration"
    This reverts commit ff7e0d6b632a21702af84ce1e6b2715f1c5df9f1.

--- local/public/wp-content/jetpack-waf/rules/allow-ip.php
+++
@@ -1,4 +0,0 @@
-<?php
-$waf_allow_list = array (
-);
-return $waf->is_ip_in_array( $waf_allow_list );
--- local/public/wp-content/jetpack-waf/rules/block-ip.php
+++
@@ -1,4 +0,0 @@
-<?php
-$waf_block_list = array (
-);
-return $waf->is_ip_in_array( $waf_block_list );
